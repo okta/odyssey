8d4bf79d617333f71f37c2e048e22440255cdb53
Arnold Sandoval <arnold.sandoval@okta.com>
2020-04-28 14:44:29 -0700

    adds vuepress-theme-nimatron

---
+++ packages/vuepress-theme-nimatron/layouts/Layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nimatron">
+    <Sidebar
+      :items="SidebarItems"
+    >
+      <template #top>
+        <slot name="Sidebar-top" />
+      </template>
+      <template #bottom>
+        <slot name="Sidebar-bottom" />
+      </template>
+    </Sidebar>
+    <main class="nimatron--main">
+      <article class="nimatron--article">
+        <transition name="fade">
+            <Content />
+        </transition>
+      </article>
+    </main>
+  </div>
+</template>
+
+
+<script>
+import { resolveSidebarItems } from '../utils'
+
+export default {
+  components: {
+    Sidebar: () => import('../components/Sidebar.vue'),
+    Example: () => import('../global-components/Example.vue'),
+  },
+  computed: {
+    SidebarItems () {
+      return resolveSidebarItems(
+        this.$page,
+        this.$page.regularPath,
+        this.$site,
+        this.$localePath
+      )
+    },
+  }
+}
+</script>
