83718975f6681d7d360b63837e50a1ec9660c340
Arnold Sandoval <arnold.sandoval@okta.com>
2020-08-17 11:31:31 -0700

    adds vue ods-icon component

---
+++ packages/docs/.vuepress/components/ods-icon.vue
@@ -0,0 +1,31 @@
+<!-- Usage: <ods-icon icon="complete" /> -->
+
+<template>
+  <div class="ods-icon" v-html="require(`!html-loader!@okta/odyssey-icons/src/${icon}.svg`)" />
+</template>
+
+<style lang="scss">
+.ods-icon {
+  display: inline-block;
+
+  svg {
+    fill: currentColor;
+    height: 24px;
+    width: 24px;
+  }
+}
+</style>
+
+<script>
+import html from '@okta/odyssey-icons/src/back.svg';
+
+export default {
+    name: 'ods-icon',
+    props: {
+        icon: {
+            type: String,
+            default: null
+        }
+    }
+}
+</script>
