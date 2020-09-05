65f974e5214baab7ea16823805d4060d33c86644
Arnold Sandoval <arnold.sandoval@okta.com>
2020-09-05 13:23:18 -0700

    add baseline odo-card component

---
+++ packages/vuepress-theme-odyssey/components/odo-card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="odo-card">
+    <slot name="header"></slot>
+    <slot></slot>
+    <slot name="footer"></slot>
+  </div>
+
+</template>
+<style lang="scss">
+@import '@okta/odyssey';
+
+.odo-card {
+  padding: $spacing-m $spacing-l;
+  border-radius: 0 32px;
+  background: cv('gray', '000');
+}
+
+</style>
+<script>
+export default {
+  name: 'odo-card'
+}
+</script>
