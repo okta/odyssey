941f190dc02883affcda10d61124ceb0d307317c
Arnold Sandoval <arnold.sandoval@okta.com>
2020-09-05 23:14:28 -0700

    improve odo-nav-vertical

--- packages/vuepress-theme-odyssey/components/odo-nav-vertical.vue
+++ packages/vuepress-theme-odyssey/components/odo-nav-vertical.vue
@@ -5,19 +5,26 @@
       [`is-ods-nav-${type}`]: type
     }"
   >
-    <ul
-      class="ods-nav-vertical--list"
-      v-for="(item, index) in nav" 
-      v-bind:key="item.name"
-    >
-      <li>
+    <ul class="ods-nav-vertical--list">
+      <li
+        class="ods-nav-vertical--list-item"
+        v-for="(item, index) in nav" 
+        v-bind:key="item.name"
+        @click="click(item.children)"
+      >
         <odo-link 
-          :id="'nav-subhead-' + index" class="ods-nav-vertical--heading" 
+          :id="'nav-subhead-' + index" 
+          class="ods-nav-vertical--heading" 
           :href="item.link"
         >
           {{item.title}}
+          <div v-if="item.children" class="ods-nav-vertical--list-sub-indicator" />
         </odo-link>
-        <ul class="ods-nav-vertical--list-sub" :aria-describedby="'nav-subhead-' + index" v-if="item.children">
+
+        <ul 
+          class="ods-nav-vertical--list-sub" 
+          :aria-describedby="'nav-subhead-' + index" v-if="item.children"
+        >
           <li 
             v-for="subitem in item.children" 
             v-bind:key="subitem.title"
@@ -38,28 +45,54 @@
 @import '@okta/odyssey';
 
 .ods-nav-vertical {
-  display: grid;
-
   a,
   a:visited {
-    display: block;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     margin-bottom: $spacing-xs;
+    padding: 0 $spacing-xs;
     color: $text-heading;
     font-size: $size-body-sentence;
     text-decoration: none;
   }
 
-  .ods-nav-vertical--heading {
+  .ods-nav-vertical--list >
+  .ods-nav-vertical--list-item {
+    &:last-child a {
+      margin-bottom: 0;
+    }
+  }
+
+  .ods-nav-vertical--list-sub-indicator {
+    width: 1em;
+    height: 1em;
+    margin-right: $spacing-xs;
+    background-image: get-icon('caret', $text-body);
   }
 
   .ods-nav-vertical--list-sub {
     display: none;
     margin-bottom: $spacing-s;
 
-    .ods-nav-vertical--list-item {
-      padding: 0 $spacing-xs;
+    .ods-nav-vertical--list-item a {
+      padding: 0 $spacing-s;
     }
   }
+
+  .router-link-active {
+    font-weight: bold;
+
+    & + .ods-nav-vertical--list-sub {
+      display: block;
+
+      .ods-nav-vertical--list-item .router-link-active {
+        background: cv('gray', '000');
+      }
+    }
+  }
+
+
 
   &.is-ods-nav-secondary {
     font-weight: bold;
@@ -85,8 +118,19 @@ export default {
       validator: (value) => ['primary', 'secondary'].includes(value)
     }
   },
+  computed: {
+
+  },
+  methods: {
+    click: (hasChildren) => {
+      if (hasChildren) {
+        // TODO: Switch attributes
+      }
+    }
+  },
   components: {
     'odo-link': () => import('./odo-link.vue'),
+    'ods-icon': () => import('../global-components/ods-icon.vue'),
   },
 }
 </script>
