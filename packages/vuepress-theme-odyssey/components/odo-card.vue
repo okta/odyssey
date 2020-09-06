bf792632aead0dfef91f254f614d8cadcb93e4fc
Arnold Sandoval <arnold.sandoval@okta.com>
2020-09-05 23:14:04 -0700

    update odo-card to add a plain theme, conditionally rendered slot elements

--- packages/vuepress-theme-odyssey/components/odo-card.vue
+++ packages/vuepress-theme-odyssey/components/odo-card.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="odo-card">
-    <slot name="header"></slot>
-    <slot></slot>
-    <slot name="footer"></slot>
+  <div :class='{
+    "odo-card": true,
+    [`is-odo-card-${type}`]: true,
+  }'>
+    <header class="odo-card--header" v-if="hasSlotHeader">
+      <slot name="header" />
+    </header>
+    <div class="odo-card--main">
+      <slot></slot>
+    </div>
+    <footer class="odo-card--footer" v-if="hasSlotFooter">
+      <slot name="footer" />
+    </footer>
   </div>
 
 </template>
@@ -13,11 +22,30 @@
   padding: $spacing-m $spacing-l;
   border-radius: 0 32px;
   background: cv('gray', '000');
+
+  &.is-odo-card-plain {
+    background: transparent;
+  }
 }
 
 </style>
 <script>
 export default {
-  name: 'odo-card'
+  name: 'odo-card',
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+      validator: (value) => ['default', 'plain'].includes(value)
+    }
+  },
+  computed: {
+    hasSlotHeader() {
+      return this.$slots.header
+    },
+    hasSlotFooter() {
+      return this.$slots.footer
+    }
+  }
 }
 </script>
