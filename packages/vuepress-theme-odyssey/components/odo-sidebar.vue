87c9345c053fdf2b3505ee7489103e55880e0b13
Arnold Sandoval <arnold.sandoval@okta.com>
2020-09-05 23:15:30 -0700

    rename slidebar slots, add WIP resize observer which applies a class if the main slot has overflow, and hack in showing/hiding sub items - these will be updated

--- packages/vuepress-theme-odyssey/components/odo-sidebar.vue
+++ packages/vuepress-theme-odyssey/components/odo-sidebar.vue
@@ -1,11 +1,16 @@
 <template>
   <header class="odo-sidebar">
     <div class="odo-sidebar--content">
-      <div class="odo-sidebar--primary">
+      <div class="odo-sidebar--header">
         <slot name="header"></slot>
+      </div>
+      <div :class='{
+        "odo-sidebar--main": true,
+        "is-overflowing": isIntersecting,
+      }'>
         <slot name="main"></slot>
       </div>
-      <div class="odo-sidebar--secondary">
+      <div class="odo-sidebar--footer">
        <slot name="footer"></slot>
       </div>
     </div>
@@ -16,38 +21,40 @@
 @import '@okta/odyssey';
 
 .odo-sidebar {
-  padding: $spacing-l $spacing-m $spacing-l $spacing-l;
+  padding: $spacing-l $spacing-m 0 $spacing-l;
 
   .odo-sidebar--content {
     display: grid;
     position: sticky;
     top: $spacing-l;
-    grid-template-areas: 'primary' 'secondary';
-    grid-template-rows: 1fr auto;
+    grid-row-gap: $spacing-s;
+    grid-template-areas:
+      'header'
+      'main'
+      'footer';
+    grid-template-rows: auto 1fr auto;
     height: 100vh;
-    max-height: calc(100vh - #{$spacing-l} * 2);
+    max-height: calc(100vh - #{$spacing-l});
+  }
+
+  .odo-sidebar--header {
+    grid-area: header;
+    padding: 0  $spacing-xs 0;
   }
 
   .odo-sidebar--main {
-    grid-area: primary;
-    padding-bottom: $spacing-m;
+    grid-area: main;
+    padding: $spacing-xs  $spacing-xs 0;
+    overflow: scroll;
+
+    &.is-overflowing {
+      border-bottom: 1px solid $border-color-display;
+    }
   }
 
-  .odo-sidebar--secondary {
-    grid-area: secondary;
-  }
-
-  header {
-    top: $spacing-m;
-  }
-
-  .odo-sidebar--title {
-    display: block;
-    margin-bottom: $spacing-m;
-    color: $text-heading;
-    font-size: $size-title-4;
-    font-weight: bold;
-    text-decoration: none;
+  .odo-sidebar--footer {
+    grid-area: footer;
+    padding: $spacing-xs  $spacing-xs $spacing-m;
   }
 }
 </style>
@@ -59,5 +66,30 @@ export default {
   components: {
     'odo-link': () => import('./odo-link.vue'),
   },
+  data: () => ({
+    isIntersecting: null,
+  }),
+  mounted () {
+    // TODO: this has to be re-thought out.
+    if('IntersectionObserver' in window) {
+      const el = this.$el.querySelector('.odo-sidebar--content');
+      // TODO: setTimeout is a hack, figure out how to do this the right way
+      setTimeout(() => {
+        let observer = new IntersectionObserver((entries, observer) => { 
+            entries.forEach(entry => {
+                if(entry.intersectionRatio !== 1){
+                 this.isIntersecting = true
+                }
+                else {
+                  this.isIntersecting = false
+                }
+            });
+        }, { threshold: 1 });
+
+        observer.observe(el.querySelectorAll('.ods-nav-vertical')[0]) ;
+      }, 500);
+
+    }
+  }
 }
 </script>
