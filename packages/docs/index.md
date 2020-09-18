8e64b94bc3e8e85d7eaff4b713fac0f46c633f6e
Arnold Sandoval <arnold.sandoval@okta.com>
2020-09-17 22:27:22 -0700

    adds wip homepage

--- packages/docs/index.md
+++ packages/docs/index.md
@@ -2,7 +2,7 @@
   template: home
   headline: Odyssey Design System
   lead: Odyssey is Okta’s design system which consists of UI’s, guides, and resources to build products.
-  actions:
+  contentPrimary:
     - title: Base
       description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
       label: See them
@@ -14,6 +14,33 @@
     - title: Icons
       description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
       label: See them
+      href: /icons/
+  principle: 
+    title: How we build
+    description: We welcome all feedback, designs, or ideas in order to produce the best possible experience for our users. If you’re interested in contributing, check out our contributing guidelines to get started.
+    label: Learn more
+    href: /base/
+  resources:
+    - title: GitHub
+      description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
+      label: Learn more
+      href: /base/
+    - title: Figma kit
+      description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
+      label: Learn more
+      href: /components/
+  contentSecondary:
+    - title: Principles
+      description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
+      label: Learn more
+      href: /base/
+    - title: Our team
+      description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
+      label: Learn more
+      href: /components/
+    - title: Get help
+      description: Et morbi eget at consectetur. Elit aenean mi phasellus. Et iusto odio atque dignissimos ducimus qui blanditiis praesentium voluptatum deleniti animi.
+      label: Learn more
       href: /icons/
 ---
 
--- packages/vuepress-theme-odyssey/templates/OdoTemplateHome.vue
+++ packages/vuepress-theme-odyssey/templates/OdoTemplateHome.vue
@@ -1,11 +1,76 @@
 <template>
   <article class="odo-main--content">
-    <h1>Odyssey Design System</h1>
+    <h1>{{ $page.frontmatter.headline }}</h1>
+    <p>{{ $page.frontmatter.lead }}</p>
+
+    <ul class="odo-grid--3col">
+      <li
+        v-for="(section, index) in $page.frontmatter.contentPrimary"
+        :key="index"
+      >
+        <OdoCard class="odo-card--index">
+          <img
+            src="/images/illustration-fpo.svg"
+            class="odo-card--index-icon"
+            aria-hidden="true"
+          />
+          <h3>{{ section.title }}</h3>
+          <p>{{ section.description }}</p>
+          <div class="odo-card--index-cta">
+            <OdoLink :href="section.href">{{ section.label }}</OdoLink>
+          </div>
+        </OdoCard>
+      </li>
+    </ul>
+
+    <div>
+      <h1>{{ $page.frontmatter.principle.title }}</h1>
+      <p style="font-size: 2.00001rem; font-weight: normal;">
+        {{ $page.frontmatter.principle.description }}
+      </p>
+      <OdoLink :href="$page.frontmatter.principle.href">{{
+        $page.frontmatter.principle.label
+      }}</OdoLink>
+    </div>
+
+    <ul class="odo-grid--2col">
+      <li v-for="(resource, index) in $page.frontmatter.resources" :key="index">
+        <OdoCard>
+          <img
+            src="/images/illustration-fpo.svg"
+            class="odo-card--index-icon"
+            aria-hidden="true"
+          />
+          <h3>{{ resource.title }}</h3>
+          <p>{{ resource.description }}</p>
+          <div class="odo-card--index-cta">
+            <OdoLink :href="resource.href">{{ resource.label }}</OdoLink>
+          </div>
+        </OdoCard>
+      </li>
+    </ul>
+
+    <ul class="odo-grid--3col">
+      <li
+        v-for="(section, index) in $page.frontmatter.contentSecondary"
+        :key="index"
+      >
+        <h3>{{ section.title }}</h3>
+        <p>{{ section.description }}</p>
+        <div class="odo-card--index-cta">
+          <OdoLink :href="section.href">{{ section.label }}</OdoLink>
+        </div>
+      </li>
+    </ul>
   </article>
 </template>
 
 <script>
 export default {
-  name: "OdoTemplateHome"
+  name: "OdoTemplateHome",
+  components: {
+    OdoLink: () => import("../components/OdoLink.vue"),
+    OdoCard: () => import("../components/OdoCard.vue")
+  }
 };
 </script>
