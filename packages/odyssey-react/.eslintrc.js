34ee01686727cce257d862077c63f3eb8532d51d
Arnold Sandoval <62259644+arnoldsandoval-okta@users.noreply.github.com>
2021-05-21 13:59:19 -0700

    feat(odyssey-react): Adds odyssey-react package with typescript, button component, storybook (#942)

---
+++ packages/odyssey-react/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended']
+}
---
+++ packages/odyssey-react/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "typescript": {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ]
+}
