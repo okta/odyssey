84b983ea203eb3f62ca248ca79dc2b05afb127ef
Jeff Belser <jeff.belser@okta.com>
2021-06-03 17:36:18 -0400

    feat(odyssey-react): add useOid hook

--- packages/odyssey-react/src/utils/index.ts
+++ packages/odyssey-react/src/utils/index.ts
@@ -10,5 +10,4 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import oid from './oid';
-export { oid };
+export { oid, useOid } from './oid';
