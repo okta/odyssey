212de7a1e66d83a01cafd1b73aba6146992ac226
Arnold Sandoval <arnold.sandoval@okta.com>
2021-06-23 09:27:29 -0700

    feat(odyssey-react): add translations for german and japanese

---
+++ packages/odyssey-react/.storybook/translations/index.js
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import ar from "./ar.json";
+import de from "./de.json";
+import en from "./en.json";
+import jp from "./jp.json";
+
+const translations = {
+  ar,
+  de,
+  en,
+  jp
+};
+
+export default translations;
