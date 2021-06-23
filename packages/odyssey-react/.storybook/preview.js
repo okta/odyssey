233186c8cb08b2ed2ec8921ec7783a14193b2087
Arnold Sandoval <arnold.sandoval@okta.com>
2021-06-23 09:27:29 -0700

    feat(odyssey-react): add translations for german and japanese

--- packages/odyssey-react/.storybook/preview.js
+++ packages/odyssey-react/.storybook/preview.js
@@ -1,4 +1,6 @@
 import "./preview.scss";
+import translations from "./translations";
+import { IntlProvider } from "react-intl";
 
 export const parameters = {
   controls: {
@@ -22,8 +24,44 @@ export const parameters = {
         "Base",
         "Components",
         "Patterns"
-      ],
-      locales: '',
+      ]
     },
   },
 };
+
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'ar', title: 'عربى (Arabic)' },
+        { value: 'de', title: 'Deutsche (German)' },
+        { value: 'en', title: 'English' },
+        { value: 'jp', title: '日本語 (Japanese)' }
+      ]
+    }
+  }
+};
+
+const setDir = (locale = 'en') => {
+  const rtlLocales = ['ar'];
+  const dir = rtlLocales.includes(locale) ? 'rtl' : 'ltr';
+
+  document.documentElement.setAttribute('dir', dir);
+}
+
+const withLocaleProvider = (Story, context) => {
+  const locale = context.globals.locale;
+  
+  setDir(locale)
+
+  return (
+    <IntlProvider locale={locale} messages={translations[locale]}>
+      <Story {...context} />
+    </IntlProvider>
+  )
+}
+export const decorators = [ withLocaleProvider ];
