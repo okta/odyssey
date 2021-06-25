8d99c07f1314228671994d1467c5c975b5190b1c
Jeff Belser <jeff.belser@okta.com>
2021-06-25 10:30:21 -0400

    refactor(odyssey-react): cleanup RadioButton and RadioGroup

---
+++ packages/odyssey-react/src/components/Radio/RadioButton/index.tsx
@@ -0,0 +1,88 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import React, { forwardRef } from 'react';
+import type { ComponentPropsWithRef } from 'react';
+import { useRadioGroup } from '../context';
+import { useOid, useOmit } from '../../../utils';
+
+export interface Props extends Omit<
+  ComponentPropsWithRef<'input'>,
+  'style' | 'className'
+> {
+  /**
+   * The underlying input element id attribute. Automatically generated if not provided
+   */
+  id?: string,
+
+  /**
+   * The form field label
+   */
+  label: string,
+
+  /**
+   * The underlying input element value attribute
+   */
+  value: string,
+}
+
+/**
+ * Radios appear as a ring shaped UI accompanied by a caption that allows
+ * the user to choose only one option at a time.
+ */
+const RadioButton = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const {
+    id,
+    label,
+    value,
+    ...rest
+  } = props;
+
+  const {
+    value: controlledValue,
+    onChange,
+    disabled,
+    required,
+    name
+  } = useRadioGroup();
+
+  const oid = useOid(id);
+
+  const omitProps = useOmit(rest);
+
+  const checked = value === controlledValue;
+
+  return (
+    <>
+      <input
+        {...omitProps}
+        className="ods-radio"
+        checked={checked}
+        disabled={disabled}
+        id={oid}
+        onChange={onChange}
+        name={name}
+        ref={ref}
+        required={required}
+        type="radio"
+        value={value}
+      />
+      <label
+        children={label}
+        className="ods-radio--label"
+        htmlFor={oid}
+      />
+    </>
+  );
+});
+
+export default RadioButton;
---
+++ packages/odyssey-react/src/components/Radio/context.tsx
@@ -0,0 +1,80 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useState
+} from 'react';
+import type { ReactElement } from 'react';
+import type { Props } from './RadioGroup';
+
+interface Context {
+  value?: string;
+  disabled?: boolean;
+  name?: string;
+  required?: boolean;
+  onChange?: Props['onChange'];
+}
+
+interface ProviderProps {
+  children: ReactElement | ReactElement[];
+  value?: Context;
+}
+
+const RadioGroupContext = createContext<Context>({});
+
+const useValue = ({ value, onChange, ...rest }: Context) => {
+  const [valueState, setValueState] = useState(value);
+
+  const onChangeWithState = useCallback(
+    (event) => {
+      onChange?.(event, event.target.value);
+      setValueState(event.target.value);
+    },
+    [onChange, setValueState]
+  );
+
+  return {
+    ...rest,
+    value: value ?? valueState,
+    onChange: onChangeWithState
+  };
+};
+
+const RadioGroupProvider = (props: ProviderProps): ReactElement => {
+  const {
+    children,
+    value = {}
+  } = props;
+
+  return <RadioGroupContext.Provider
+    value={useValue(value)}
+    children={children}
+  />;
+};
+
+const useRadioGroup = (): Context => {
+  const context = useContext(RadioGroupContext);
+
+  if (Object.keys(context).length === 0) {
+    throw new Error('useRadioGroup must be used within a RadioGroupProvider');
+  }
+
+  return context;
+};
+
+export {
+  RadioGroupProvider,
+  useRadioGroup
+};
