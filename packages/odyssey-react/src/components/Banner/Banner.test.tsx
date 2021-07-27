f0acd263729a675c1bda0ebe3191decdfa1ba3e7
Arnold Sandoval <62259644+arnoldsandoval-okta@users.noreply.github.com>
2021-07-27 13:42:48 -0700

    feat(odyssey-react): add banner component (#1002)
    * feat(odyssey-react): add banner component

---
+++ packages/odyssey-react/src/components/Banner/Banner.test.tsx
@@ -0,0 +1,127 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Banner from ".";
+
+const role = "status";
+const bannerTitle = "Banner title";
+const bannerContent = "Banner content";
+const bannerActions = "Banner actions <a href='https://www.okta.com'>with a link</a>";
+const bannerDismissButtonLabel = "Dismiss message";
+
+describe("Banner", () => {
+  it('renders the Banner', () => {
+    const { getByRole } = render(
+      <Banner open title={bannerTitle} content={bannerContent}>
+        {bannerActions}
+      </Banner>
+    );
+
+    expect(getByRole(role)).toBeInTheDocument();
+  });
+
+  it('renders the Banner title', () => {
+    const { getByText } = render(
+      <Banner open title={bannerTitle} content={bannerContent}>
+        {bannerActions}
+      </Banner>
+    );
+
+    expect(getByText(bannerTitle)).toBeInTheDocument();
+  });
+
+  it('renders the content', () => {
+    const { getByText } = render(
+      <Banner open title={bannerTitle} content={bannerContent}>
+        {bannerActions}
+      </Banner>
+    );
+    expect(getByText(bannerContent)).toBeInTheDocument();
+  });
+
+  it('renders the action (children) content', () => {
+    const { getByText } = render(
+      <Banner open title={bannerTitle} content={bannerContent}>
+        {bannerActions}
+      </Banner>
+    );
+    expect(getByText(bannerActions)).toBeInTheDocument();
+  });
+
+
+  it('hides the banner if visible prop is set to false', () => {
+    const { getByRole } = render(
+      <Banner open={false} title={bannerTitle} content={bannerContent}>
+        {bannerActions}
+      </Banner>
+    );
+
+    expect(getByRole(role)).toHaveClass('is-ods-banner-dismissed');
+  });
+
+  it('should NOT display the dismiss button when the onDismiss callback fn is NOT provided', () => {
+    const { queryByRole } = render(
+      <Banner open title={bannerTitle} content={bannerContent}>
+        {bannerActions}
+      </Banner>
+    );
+
+    expect(queryByRole('button')).toBeNull();
+  });
+  
+  it('should display the dismiss button when the onDismiss callback fn is provided', () => {
+    const handleDismiss = jest.fn();
+    const { getByRole } = render(
+      <Banner open title={bannerTitle} content={bannerContent} onDismiss={handleDismiss} dismissButtonLabel={bannerDismissButtonLabel}>
+        {bannerActions}
+      </Banner>
+    );
+    expect(getByRole('button')).toBeInTheDocument();
+  });
+  
+  it('should call onDismiss when the dismiss button is clicked', () => {
+    const handleDismiss = jest.fn();
+    const { getByRole } = render(
+      <Banner open title={bannerTitle} content={bannerContent} onDismiss={handleDismiss} dismissButtonLabel={bannerDismissButtonLabel}>
+        {bannerActions}
+      </Banner>
+    );
+
+    fireEvent.click(getByRole('button'));
+    expect(handleDismiss).toHaveBeenCalledTimes(1);
+  });
+
+  it('should update the aria-label attribute on the dismiss button if dismissButtonLabel is provided', () => {
+    const handleDismiss = jest.fn();
+    const { getByRole } = render(
+      <Banner 
+        dismissButtonLabel={bannerDismissButtonLabel} 
+        open 
+        title={bannerTitle} 
+        content={bannerContent} 
+        onDismiss={handleDismiss}
+      >
+        {bannerActions}
+      </Banner>
+    );
+
+    expect(getByRole('button')).toHaveAttribute('aria-label', bannerDismissButtonLabel);
+  });
+
+  a11yCheck(() => render(
+    <Banner open title={bannerTitle} content={bannerContent}>
+      {bannerActions}
+     </Banner>
+    ))
+});
---
+++ packages/odyssey-react/src/components/Banner/index.tsx
@@ -0,0 +1,119 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import React from 'react';
+import type { FunctionComponent, ReactNode } from 'react';
+import { useCx, useOmit } from '../../utils';
+import Button from '../Button';
+
+export type BannerVariants = 'info' | 'danger' | 'caution';
+interface ComponentProps {
+  /**
+   * Actions, or links to be rendered on the right side of 
+   * the component.
+   */
+  children: ReactNode;
+  
+  /**
+   * The visual variant to be displayed to the user.
+   * @default info
+   */
+  variant?: BannerVariants;
+
+  /**
+   * Human-readable title for the banner.
+   */
+  title: string;
+
+  /**
+   * Human-readable descriptive content for the banner.
+   */
+  content: string;
+
+  /**
+   * Determines whether the banner should be displayed to the user.
+   */
+  open: boolean;
+}
+
+type DismissableComponentProps =
+| { onDismiss?: never; dismissButtonLabel?: never }
+| { onDismiss: () => void; dismissButtonLabel: string }
+
+
+export type Props = ComponentProps & DismissableComponentProps;
+
+/**
+ * Banners let users know important messages related to their overall experience
+ * on the website. They can be purely informational messages or critical errors 
+ * to act upon.
+ *
+ * @component
+ * @example
+ * <Banner 
+ *  variant="primary"
+ *  open={isOpen}
+ *  title="New launch scheduled"
+ *  content="The mission to Sagitarius A has been set for January 7."
+ *  onDismiss={handleBannerDismiss}
+ * >
+ *    <Link href="/fuel">Visit fueling console</Link>
+ * </Banner>
+ */
+
+ const Banner: FunctionComponent<Props> = (props) => {
+  const {
+    children,
+    title,
+    content,
+    open,
+    variant = "info",
+    onDismiss,
+    dismissButtonLabel,
+    ...rest
+  } = props;
+
+  const componentClass = useCx(
+    "ods-banner",
+    variant && `is-ods-banner-${variant}`,
+    !open && "is-ods-banner-dismissed",
+    onDismiss && "is-ods-banner-dismissable"
+  );
+
+  const omitProps = useOmit(rest);
+
+  return (
+    <aside
+      {...omitProps}
+      className={componentClass}
+      role="status"
+    >
+      <span className="ods-banner--icon">
+        {/* @todo Insert <Icon> component */}
+        &#8253;
+      </span>
+      {title && <h1 className="ods-banner--title">{title}</h1>}
+      {content && <p className="ods-banner--content">{content}</p>}
+      {children && <section className="ods-banner--actions">{children}</section>}
+      {onDismiss &&
+        <span className="ods-banner--dismiss">
+          <Button variant="dismiss" onClick={onDismiss} aria-label={dismissButtonLabel}>
+            {/* @todo Insert <Icon> component, dismiss variant */}
+            &#8253;
+          </Button>
+        </span>
+      }
+    </aside>
+  );
+};
+
+export default Banner;
