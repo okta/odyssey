170e7218dd28c5b2dc3705cc318fe8970a0acee3
Jeff Belser <jeff.belser@okta.com>
2021-08-02 17:10:19 -0400

    feat(odyssey-react): allow storybook to consume scss module

--- packages/odyssey-react/.storybook/main.js
+++ packages/odyssey-react/.storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
-  "typescript": {
+  typescript: {
     check: false,
     checkOptions: {},
     reactDocgen: 'react-docgen-typescript',
@@ -11,10 +11,44 @@ module.exports = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-scss",
-    "@storybook/addon-a11y"
-  ]
-}
+    "@storybook/addon-a11y",
+  ],
+  webpackFinal(config) {
+    config.module.rules.push(
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              modules: {
+                auto: true,
+                localIdentName: "[folder]__[local]__[hash:base64:5]",
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              additionalData: `
+                @import '~@okta/odyssey/src/scss/abstracts/functions';
+                @import '~@okta/odyssey/src/scss/abstracts/colors';
+                @import '~@okta/odyssey/src/scss/abstracts/mixins';
+                @import '~@okta/odyssey/src/scss/abstracts/tokens';
+              `
+            }
+          }
+        ],
+      }
+    );
+
+    return config;
+  }
+};
