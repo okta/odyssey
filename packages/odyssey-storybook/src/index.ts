76fd41c588e8ba71d6afe0e05e8457f3a75e730a
Andrew Berg <87656589+andrewberg-okta@users.noreply.github.com>
2021-08-23 11:09:37 -0400

    feat(odyssey-react): add select scss module

--- packages/odyssey-storybook/src/index.ts
+++ packages/odyssey-storybook/src/index.ts
@@ -71,7 +71,6 @@ module.exports = {
                 @import '@okta/odyssey/src/scss/components/input-field-layout';
                 @import '@okta/odyssey/src/scss/components/label';
                 @import '@okta/odyssey/src/scss/components/number-input';
-                @import '@okta/odyssey/src/scss/components/select';
                 @import '@okta/odyssey/src/scss/components/toast';
                 @import '@okta/odyssey/src/scss/components/toast-pen';
               `
