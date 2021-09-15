b3bc41d23ad5337b7abec573764cdad3a96fb985
Arnold Sandoval <arnold.sandoval@okta.com>
2021-09-15 13:52:00 -0700

    chore(odyssey-react): add tab keybaord tests

--- packages/odyssey-react/src/components/Tab/Tab.test.tsx
+++ packages/odyssey-react/src/components/Tab/Tab.test.tsx
@@ -10,7 +10,8 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { render, fireEvent } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Tabs from ".";
 
 const roleTabList = "tablist";
