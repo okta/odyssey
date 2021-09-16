db681ecbc34d1e0fe875d59547ed9ee019a987a4
Andrew Berg <87656589+andrewberg-okta@users.noreply.github.com>
2021-09-16 18:29:44 -0400

    feat(odyssey-react): add List component with styles and sub-components

--- packages/odyssey-react/src/components/List/List.tsx
+++ packages/odyssey-react/src/components/List/List.tsx
@@ -10,32 +10,77 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import type { FunctionComponent } from 'react';
+import { FC } from 'react';
+import type {
+  ReactElement,
+  ComponentProps
+} from 'react';
+
+import {
+  useOmit,
+  useCx
+} from '../../utils';
+
+import styles from './List.module.scss';
+import ListItem from './ListItem';
+import DescriptionTerm from './DescriptionTerm';
+import DescriptionDetails from './DescriptionDetails';
 
 export type Props = {
-  type?: 'unordered' | 'ordered' | 'definition'
-};
+  /**
+  * List Items
+  */
+  children?: ReactElement | ReactElement[],
+  /**
+   * List type
+   */
+   listType?: 'unordered' | 'ordered' | 'description',
+   /**
+    * Remove default styling
+    */
+   unstyled?: boolean,
+} & ComponentProps<'ul'> & ComponentProps<'ol'> & ComponentProps<'dl'>;
 
-const List: FunctionComponent<Props> = (props) => {
+type StaticComponents = {
+  Item: typeof ListItem,
+  Term: typeof DescriptionTerm,
+  Details: typeof DescriptionDetails,
+}
+
+const List: FC<Props> & StaticComponents = (props:Props):ReactElement => {
   const {
-    type = 'unordered',
     children,
-  } = props;
+    listType = 'unordered',
+    unstyled = false,
+    ...rest
+  } = props
 
-  let ListElement: keyof JSX.IntrinsicElements = 'ul';
+  const omitProps = useOmit(rest);
+
+  const componentClass = useCx(
+    !unstyled && styles.root,
+    unstyled ? styles.unstyled : styles[listType ],
+  );
+
+  type ListTag = 'ul' | 'ol' | 'dl';
+  let ListElement: ListTag = 'ul';
   
-  if(type === 'ordered'){
+  if(listType === 'ordered'){
     ListElement = 'ol';
   }
-  else if(type === 'definition'){
+  else if(listType === 'description'){
     ListElement = 'dl';
   }
 
   return (
-    <ListElement>
+    <ListElement className={componentClass} {...omitProps}>
       {children}
-    </ListElement>
+    </ListElement >
   )
 }
+
+List.Item = ListItem;
+List.Term = DescriptionTerm;
+List.Details = DescriptionDetails;
 
 export default List;
