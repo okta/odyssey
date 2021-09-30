b7523243ee48ccef905049104333d2f00053b025
Jeff Belser <63716167+jeffbelser-okta@users.noreply.github.com>
2021-09-30 14:54:45 -0400

    feat(odyssey-storybook): withStyles Infobox

--- packages/odyssey-storybook/src/index.ts
+++ packages/odyssey-storybook/src/index.ts
@@ -17,7 +17,7 @@ import sass from "sass";
 import postcss from "postcss";
 
 const isProduction = process.env.NODE_ENV === "production";
-const withStyles = /\/(Banner|Button|Checkbox)\.module\.scss$/;
+const withStyles = /\/(Banner|Button|Checkbox|Infobox)\.module\.scss$/;
 
 module.exports = {
   typescript: {
