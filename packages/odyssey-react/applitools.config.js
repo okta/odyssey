62a70107151a3bdb4433cc6f7ea2782ca1d1d973
Arnold Sandoval <62259644+arnoldsandoval-okta@users.noreply.github.com>
2021-10-05 16:56:01 -0700

    chore: add applitools dep, separate tests from docs-deployment gh workflow (#1081)
    * chore: add vrt, rename workflow to ci, breakout tests, vrt and notifications in to separate jobs

---
+++ packages/odyssey-react/applitools.config.js
@@ -0,0 +1,27 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+module.exports = {
+  testConcurrency: 10,
+  browser: [
+    { width: 1024, height: 768, name: "ie11" },
+    { width: 1024, height: 768, name: "edgechromium" },
+    { width: 1024, height: 768, name: "chrome" },
+    { width: 1024, height: 768, name: "safari" },
+    { width: 1024, height: 768, name: "firefox" },
+  ],
+  accessibilityValidation: {
+    level: "AA",
+    guidelinesVersion: "WCAG_2_1",
+  },
+  matchLevel: "Strict",
+};
