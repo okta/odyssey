1e64a78fe0598cc0d211206e43b3cfd3840d98dd
Andrew Berg <87656589+andrewberg-okta@users.noreply.github.com>
2021-10-14 17:39:54 -0400

    feat(odyssey-react): add withStyles to Status

---
+++ packages/odyssey-react/src/components/Status/Status.tsx
@@ -0,0 +1,74 @@
+/*!
+ * Copyright (c) 2021-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import type { ComponentProps, ComponentPropsWithoutRef } from "react";
+import { forwardRef } from "react";
+import { useCx, withStyles } from "../../utils";
+import styles from "./Status.module.scss";
+
+import ScreenReaderText from "../ScreenReaderText";
+
+export type StatusVariants = "neutral" | "success" | "caution" | "danger";
+
+interface Props
+  extends Omit<ComponentPropsWithoutRef<"div">, "style" | "className"> {
+  /**
+   * The status label.
+   */
+  label: string;
+
+  /**
+   * Visually hides the status label.
+   */
+  labelHidden?: boolean;
+
+  /**
+   * The description of the present state/status.
+   */
+  descriptor: string;
+
+  /**
+   * The visual variant to be displayed to the user.
+   * @default neutral
+   */
+  variant?: StatusVariants;
+}
+
+/**
+ * Status is used to inform users by providing feedback on system states. Status can display broad
+ * operational states as well as granular states like user status.
+ *
+ * @component
+ * @example <Status label={label} descriptor={descriptor} />
+ */
+let Status = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { label, descriptor, labelHidden = false, variant = "neutral" } = props;
+
+  const valueClass = useCx(styles.value, styles[`${variant}Variant`]);
+
+  return (
+    <div className={styles.status} role="status" ref={ref}>
+      {!labelHidden && <span className={styles.label}>{label}</span>}
+      {labelHidden && <ScreenReaderText>{label}</ScreenReaderText>}
+      <span className={valueClass}>{descriptor}</span>
+    </div>
+  );
+});
+
+Status.displayName = "Status";
+
+Status = withStyles(styles)(Status);
+
+type StatusProps = ComponentProps<typeof Status>;
+export type { StatusProps as Props };
+
+export default Status;
