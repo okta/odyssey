5176f5e09b8728dfa0a7003e008a95126de70bad
Jeff Belser <63716167+jeffbelser-okta@users.noreply.github.com>
2021-10-15 15:58:39 -0400

    chore(odyssey-react): rename Icon to IconDictionary

--- packages/odyssey-react/src/components/Icon/IconDictionary.test.tsx
+++ packages/odyssey-react/src/components/Icon/IconDictionary.test.tsx
@@ -11,16 +11,17 @@
  */
 
 import { render, screen } from "@testing-library/react";
-import Icon from "./Icon";
+import { IconDictionary } from "./IconDictionary";
 
 const iconTitle = "Icon";
 
 describe("Icon", () => {
-  it("render the named icon", () => {
-    render(<Icon name="check" title={iconTitle} />);
-    const svgElement = screen.getByTitle(iconTitle).parentElement;
+  it("render the named icon visibly into document", () => {
+    render(<IconDictionary name="check" title={iconTitle} />);
+    const svgElement = screen.getByRole("img");
     expect(svgElement).toBeVisible();
+    expect(svgElement.querySelector("title")).toHaveTextContent(iconTitle);
   });
 
-  a11yCheck(() => render(<Icon name="check" title="test" />));
+  a11yCheck(() => render(<IconDictionary name="check" title="test" />));
 });
