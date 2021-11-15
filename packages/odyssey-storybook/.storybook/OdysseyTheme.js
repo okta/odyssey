48ac55499303614b82fc030fb98c2f5507313b9e
Edbury Enegren <edbury.enegren@okta.com>
2021-11-15 10:33:28 -0800

    docs: adds an Odyssey theme for Storybook

---
+++ packages/odyssey-storybook/.storybook/OdysseyTheme.js
@@ -0,0 +1,37 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  colorPrimary: '#1662dd',
+  colorSecondary: '#00297a',
+
+  // UI
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  appBorderColor: '#d7d7dc',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Public Sans", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", "Noto Sans Arabic", sans-serif',
+  fontCode: '"Inconsolata", "SFMono-Regular", "Consolas", "Liberation Mono", "Menlo", "Courier", monospace',
+
+  // Text colors
+  textColor: '#1d1d21',
+  textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
+  barTextColor: '#1662dd',
+  barSelectedColor: '#1d1d21',
+  barBg: '#f5f5f6',
+
+  // Form colors
+  inputBg: '#ffffff',
+  inputBorder: '#8c8c96',
+  inputTextColor: '#1d1d21',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Odyssey Design System',
+  //brandUrl: 'https://odyssey.okta.com',
+  //brandImage: 'https://place-hold.it/350x150',
+});
---
+++ packages/odyssey-storybook/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import OdysseyTheme from './OdysseyTheme';
+
+addons.setConfig({
+  theme: OdysseyTheme,
+});
