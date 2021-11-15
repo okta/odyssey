2e240319c5575c9953c24cf747f398782c4f9627
Edbury Enegren <edbury.enegren@okta.com>
2021-11-15 15:05:31 -0800

    fix(odyssey-design-tokens): call "value" instead of whole objects

--- packages/odyssey-storybook/.storybook/OdysseyTheme.js
+++ packages/odyssey-storybook/.storybook/OdysseyTheme.js
@@ -1,36 +1,35 @@
 import { create } from "@storybook/theming";
+import { ColorPrimaryBase, ColorPrimaryDark, ColorPaletteNeutral000, ColorPaletteNeutralWhite, ColorPaletteNeutral200, FontFamilyBase, FontFamilyMono, ColorTextBody, ColorTextBodyInverse, ColorPaletteNeutral600, ColorPaletteNeutral500 } from "@okta/odyssey-design-tokens";
 
 export default create({
   base: "light",
 
-  colorPrimary: "#1662dd",
-  colorSecondary: "#00297a",
+  colorPrimary: ColorPrimaryBase,
+  colorSecondary: ColorPrimaryDark,
 
   // UI
-  appBg: "#f5f5f6",
-  appContentBg: "#ffffff",
-  appBorderColor: "#d7d7dc",
+  appBg: ColorPaletteNeutral000,
+  appContentBg: ColorPaletteNeutralWhite,
+  appBorderColor: ColorPaletteNeutral200,
   appBorderRadius: 4,
 
   // Typography
-  fontBase:
-    '"Public Sans", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", "Noto Sans Arabic", sans-serif',
-  fontCode:
-    '"Inconsolata", "SFMono-Regular", "Consolas", "Liberation Mono", "Menlo", "Courier", monospace',
+  fontBase: FontFamilyBase,
+  fontCode: FontFamilyMono,
 
   // Text colors
-  textColor: "#1d1d21",
-  textInverseColor: "#ffffff",
+  textColor: ColorTextBody,
+  textInverseColor: ColorTextBodyInverse,
 
   // Toolbar default and active colors
-  barTextColor: "#6e6e78",
-  barSelectedColor: "#1d1d21",
-  barBg: "#ffffff",
+  barTextColor: ColorPaletteNeutral600,
+  barSelectedColor: ColorTextBody,
+  barBg: ColorPaletteNeutralWhite,
 
   // Form colors
-  inputBg: "#ffffff",
-  inputBorder: "#8c8c96",
-  inputTextColor: "#1d1d21",
+  inputBg: ColorPaletteNeutralWhite,
+  inputBorder: ColorPaletteNeutral500,
+  inputTextColor: ColorTextBody,
   inputBorderRadius: 4,
 
   brandTitle: "Odyssey Design System",
