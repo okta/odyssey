a795c66e9a52429e77a397fe2ff68c93b246e2d4
Jeff Belser <63716167+jeffbelser-okta@users.noreply.github.com>
2022-01-31 15:16:11 -0500

    refactor(odyssey-react)!: rename TextInput inputRef prop to ref

--- packages/odyssey-react/src/components/TextInput/TextInput.tsx
+++ packages/odyssey-react/src/components/TextInput/TextInput.tsx
@@ -10,12 +10,11 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import React, { useCallback } from "react";
+import React, { forwardRef, useCallback } from "react";
 import type {
-  FunctionComponent,
   FocusEventHandler,
+  ComponentPropsWithRef,
   ChangeEvent,
-  RefCallback,
 } from "react";
 import { withTheme } from "@okta/odyssey-react-theme";
 import { useOid, useOmit, useCx } from "../../utils";
@@ -25,7 +24,12 @@ import type { CommonFieldProps } from "../Field/types";
 import { theme } from "./TextInput.theme";
 import styles from "./TextInput.module.scss";
 
-export interface TextInputProps extends CommonFieldProps {
+export interface TextInputProps
+  extends CommonFieldProps,
+    Omit<
+      ComponentPropsWithRef<"input">,
+      "style" | "className" | "color" | "onChange"
+    > {
   /**
    * The underlying input element id attribute. Automatically generated if not provided
    */
@@ -36,12 +40,6 @@ export interface TextInputProps extends CommonFieldProps {
    * @default text
    */
   type?: "text" | "email" | "url" | "tel" | "search" | "password";
-
-  /**
-   * Callback to provide a reference to the underlying input element
-   * @param {Object} instance the input element or null
-   */
-  inputRef?: RefCallback<HTMLInputElement>;
 
   /**
    * The underlying input element name attribute
@@ -98,89 +96,90 @@ export interface TextInputProps extends CommonFieldProps {
 /**
  * Text inputs allow users to edit and input data.
  */
-export const TextInput: FunctionComponent<TextInputProps> = withTheme(
+export const TextInput = withTheme(
   theme,
   styles
-)((props) => {
-  const {
-    defaultValue,
-    disabled = false,
-    id,
-    inputRef,
-    name,
-    onBlur,
-    onChange,
-    onFocus,
-    placeholder,
-    readonly = false,
-    required,
-    type = "text",
-    value,
-    error,
-    hint,
-    label,
-    optionalLabel,
-    ...rest
-  } = props;
+)(
+  forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+    const {
+      defaultValue,
+      disabled = false,
+      id,
+      name,
+      onBlur,
+      onChange,
+      onFocus,
+      placeholder,
+      readonly = false,
+      required,
+      type = "text",
+      value,
+      error,
+      hint,
+      label,
+      optionalLabel,
+      ...rest
+    } = props;
 
-  const oid = useOid(id);
-  const omitProps = useOmit(rest);
+    const oid = useOid(id);
+    const omitProps = useOmit(rest);
 
-  const handleChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      onChange?.(event, event.target.value);
-    },
-    [onChange]
-  );
+    const handleChange = useCallback(
+      (event: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(event, event.target.value);
+      },
+      [onChange]
+    );
 
-  const ariaDescribedBy = useCx(
-    hint && `${oid}-hint`,
-    typeof error !== "undefined" && `${oid}-error`
-  );
+    const ariaDescribedBy = useCx(
+      hint && `${oid}-hint`,
+      typeof error !== "undefined" && `${oid}-error`
+    );
 
-  const input = (
-    <input
-      aria-describedby={ariaDescribedBy}
-      className={styles.root}
-      disabled={disabled}
-      id={oid}
-      name={name}
-      onChange={handleChange}
-      onBlur={onBlur}
-      onFocus={onFocus}
-      placeholder={placeholder}
-      readOnly={readonly}
-      ref={inputRef}
-      required={required}
-      type={type}
-      defaultValue={defaultValue}
-      value={value}
-      {...omitProps}
-    />
-  );
+    const input = (
+      <input
+        {...omitProps}
+        aria-describedby={ariaDescribedBy}
+        className={styles.root}
+        disabled={disabled}
+        id={oid}
+        name={name}
+        onChange={handleChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        placeholder={placeholder}
+        readOnly={readonly}
+        ref={ref}
+        required={required}
+        type={type}
+        defaultValue={defaultValue}
+        value={value}
+      />
+    );
 
-  const search = (
-    <span className={styles.outer}>
-      {input}
-      <span className={styles.indicator} role="presentation">
-        <SearchIcon />
+    const search = (
+      <span className={styles.outer}>
+        {input}
+        <span className={styles.indicator} role="presentation">
+          <SearchIcon />
+        </span>
       </span>
-    </span>
-  );
+    );
 
-  return (
-    <Field
-      error={error}
-      hint={hint}
-      inputId={oid}
-      label={label}
-      labelHidden={type === "search"}
-      optionalLabel={optionalLabel}
-      required={required}
-    >
-      {type === "search" ? search : input}
-    </Field>
-  );
-});
+    return (
+      <Field
+        error={error}
+        hint={hint}
+        inputId={oid}
+        label={label}
+        labelHidden={type === "search"}
+        optionalLabel={optionalLabel}
+        required={required}
+      >
+        {type === "search" ? search : input}
+      </Field>
+    );
+  })
+);
 
 TextInput.displayName = "TextInput";
