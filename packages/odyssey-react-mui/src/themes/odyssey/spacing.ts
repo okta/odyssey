d90c7bcb948674766de491614e184a530249581b
Edbury Enegren <36284167+edburyenegren-okta@users.noreply.github.com>
2022-06-03 11:44:50 -0700

    chore(odyssey-react-mui): add initial theme mappings

---
+++ packages/odyssey-react-mui/src/themes/odyssey/spacing.ts
@@ -0,0 +1,28 @@
+/*!
+ * Copyright (c) 2022-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import { createTheme } from "@mui/material/styles";
+
+const themeSpacing = createTheme({
+  spacing: [
+    0,
+    "0.28571429rem",
+    "0.57142857rem",
+    "0.85714286rem",
+    "1.14285714rem",
+    "1.71428571rem",
+    "2.28571429rem",
+    "2.85714286rem",
+    "3.42857143rem",
+    "4rem",
+  ],
+});
