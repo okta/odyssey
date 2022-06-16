7eadee07c0946d2f8b07f6e4cdade64078e0b765
Odyssey Okta <103765464+odyssey-okta@users.noreply.github.com>
2022-06-16 13:04:22 -0400

    chore(odyssey-react-mui): use tokens for theme

--- packages/odyssey-storybook/.storybook/components/MuiThemeDecorator.tsx
+++ packages/odyssey-storybook/.storybook/components/MuiThemeDecorator.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@storybook/theming";
-import { theme } from "@okta/odyssey-react-mui";
+import { odysseyTheme as theme } from "@okta/odyssey-react-mui";
 import type { DecoratorFn } from "@storybook/react";
 
 export const MuiThemeDecorator: DecoratorFn = (Story) => (
