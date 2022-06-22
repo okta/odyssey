01cef5c3decf4eb55be90597848d25269b99f8e9
Odyssey Okta <103765464+odyssey-okta@users.noreply.github.com>
2022-06-22 12:45:09 -0400

    build(odyssey-react-mui): update peer deps for react 18 and jsx transform

--- packages/odyssey-react-mui/src/components/PasswordInput/PasswordInput.test.tsx
+++ packages/odyssey-react-mui/src/components/PasswordInput/PasswordInput.test.tsx
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import { a11yCheck } from "../../../test";
 import { PasswordInput } from ".";
