a562bd2543f41a99398f130adff98117ba68f1ad
Edbury Enegren <36284167+edburyenegren-okta@users.noreply.github.com>
2022-11-04 15:07:46 -0700

    refactor(odyssey-react-mui): update Tag padding, border-radius

--- packages/odyssey-react-mui/src/theme/components.tsx
+++ packages/odyssey-react-mui/src/theme/components.tsx
@@ -383,11 +383,15 @@ export const components: ThemeOptions["components"] = {
     styleOverrides: {
       root: ({ theme, ownerState }) => ({
         height: "auto",
-        paddingBlock: theme.spacing(1),
-        paddingInline: theme.spacing(2),
+        paddingBlock: theme.spacing(2),
+        paddingInline: theme.spacing(3),
         fontSize: theme.typography.body1.fontSize,
-        borderRadius: theme.mixins.borderRadius,
+        borderRadius: "1.5em",
         backgroundColor: theme.palette.grey[100],
+
+        ...(ownerState.onDelete && {
+          paddingInlineEnd: theme.spacing(2),
+        }),
 
         [`& .${chipClasses.deleteIcon}`]: {
           WebkitTapHighlightColor: "transparent",
