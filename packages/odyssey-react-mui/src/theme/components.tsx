54ef4861a352697b6bce089ca99bcec2935cabdf
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-01-18 17:02:54 -0500

    feat: re-exported MUI List components as Odyssey MUI components

--- packages/odyssey-react-mui/src/theme/components.tsx
+++ packages/odyssey-react-mui/src/theme/components.tsx
@@ -927,6 +927,13 @@ export const components: ThemeOptions["components"] = {
       },
     ],
   },
+  MuiListItem: {
+    styleOverrides: {
+      root: {
+        textAlign: "start",
+      },
+    },
+  },
   MuiListSubheader: {
     styleOverrides: {
       root: ({ theme }) => ({
