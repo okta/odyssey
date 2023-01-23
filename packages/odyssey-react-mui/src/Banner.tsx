af7883b3c54257e5deeeca6db6ff9e62580c9694
Jordan Koschei <jordan.koschei@okta.com>
2023-01-23 12:26:15 -0500

    fix: remove vestigial AlertProps import

--- packages/odyssey-react-mui/src/Banner.tsx
+++ packages/odyssey-react-mui/src/Banner.tsx
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { AlertColor, AlertProps } from "@mui/material";
+import { AlertColor } from "@mui/material";
 import { SyntheticEvent } from "react";
 import { Alert, Link } from "./";
 
