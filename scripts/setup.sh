f05ec7847ea2e552f6232ff86da6e8b815d80b25
Kevin Ghadyani <97464104+KevinGhadyani-Okta@users.noreply.github.com>
2023-02-13 13:51:03 -0600

    merge v0.19.0 into develop (#1692)
    Merge master into develop

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-NODE_VERSION=v16.17.1
+NODE_VERSION=16.19.0
 
 # Note: Yarn will automatically switch over to yarn 3 after installing yarn 1.x
 YARN_VERSION=1.22.19
 
+export ORIGINAL_REPO=$REPO
+export CURRENT_DIR=$(pwd)
+REPO=odyssey
+
+cd ${OKTA_HOME}/${REPO}
+
 echo "installing node ${NODE_VERSION}"
-if setup_service node-and-yearn $NODE_VERSION $YARN_VERSION; then
+
+if setup_service node-and-yarn $NODE_VERSION $YARN_VERSION; then
   echo "Installed node ${NODE_VERSION} and yarn $YARN_VERSION successfully"
 else
   echo "node ${NODE_VERSION} and yarn $YARN_VERSION installation failed."
 fi
-
-cd ${OKTA_HOME}/odyssey
 
 # Override .yarnrc.yml npmRegistryServer with Okta's
 export YARN_NPM_REGISTRY_SERVER=${ARTIFACTORY_URL}/api/npm/npm-okta-master
@@ -21,3 +26,6 @@ if ! yarn install --immutable; then
   echo "yarn install command failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
+
+REPO=$ORIGINAL_REPO
+cd $CURRENT_DIR
