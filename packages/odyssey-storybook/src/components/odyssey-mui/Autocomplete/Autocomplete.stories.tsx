afd5196b281598bb6d69b1d1b8ba642e5c26ede9
Edbury Enegren <36284167+edburyenegren-okta@users.noreply.github.com>
2023-03-29 12:49:43 -0700

    chore: remove defaults from stories

--- packages/odyssey-storybook/src/components/odyssey-mui/Autocomplete/Autocomplete.stories.tsx
+++ packages/odyssey-storybook/src/components/odyssey-mui/Autocomplete/Autocomplete.stories.tsx
@@ -25,41 +25,7 @@ const storybookMeta: ComponentMeta<typeof Autocomplete> = {
     },
   },
   argTypes: {
-    // autoComplete appears to be broken in both examples
-    autoComplete: {
-      control: "boolean",
-      defaultValue: false,
-    },
-    autoHighlight: {
-      control: "boolean",
-      defaultValue: true,
-    },
-    autoSelect: {
-      control: "boolean",
-      defaultValue: false,
-    },
-    blurOnSelect: {
-      control: "radio",
-      options: [true, false, "mouse", "touch"],
-      defaultValue: false,
-    },
-    clearOnBlur: {
-      control: "boolean",
-      defaultValue: false,
-    },
-    clearOnEscape: {
-      control: "boolean",
-      defaultValue: true,
-    },
-    disableCloseOnSelect: {
-      control: "boolean",
-      defaultValue: false,
-    },
     disabled: {
-      control: "boolean",
-      defaultValue: false,
-    },
-    filterSelectedOptions: {
       control: "boolean",
       defaultValue: false,
     },
@@ -67,21 +33,9 @@ const storybookMeta: ComponentMeta<typeof Autocomplete> = {
       control: "boolean",
       defaultValue: false,
     },
-    fullWidth: {
-      control: "boolean",
-      defaultValue: false,
-    },
-    handleHomeEndKeys: {
-      control: "boolean",
-      defaultValue: true,
-    },
     includeInputInList: {
       control: "boolean",
       defaultValue: false,
-    },
-    limitTags: {
-      control: "text",
-      defaultValue: "-1",
     },
     loading: {
       control: "boolean",
@@ -91,17 +45,9 @@ const storybookMeta: ComponentMeta<typeof Autocomplete> = {
       control: "boolean",
       defaultValue: false,
     },
-    openOnFocus: {
-      control: "boolean",
-      defaultValue: false,
-    },
     readOnly: {
       control: "boolean",
       defaultValue: false,
-    },
-    selectOnFocus: {
-      control: "boolean",
-      defaultValue: true,
     },
   },
   decorators: [MuiThemeDecorator],
@@ -289,45 +235,10 @@ const MuiTemplate: ComponentStory<typeof Autocomplete> = (args) => {
 export const Default = Template.bind({});
 Default.args = {};
 
-export const autoHighlight = Template.bind({});
-autoHighlight.args = {
-  autoHighlight: true,
-};
-
-export const autoSelect = Template.bind({});
-autoSelect.args = {
-  autoSelect: true,
-};
-
-export const blurOnSelect = Template.bind({});
-blurOnSelect.args = {
-  blurOnSelect: true,
-};
-
-export const clearOnBlur = Template.bind({});
-clearOnBlur.args = {
-  clearOnBlur: true,
-};
-
-export const clearOnEscape = Template.bind({});
-clearOnEscape.args = {
-  clearOnEscape: true,
-};
-
-export const disableCloseOnSelect = Template.bind({});
-disableCloseOnSelect.args = {
-  disableCloseOnSelect: true,
-};
-
 export const disabled = Template.bind({});
 disabled.args = {
   disabled: true,
   value: "The Godfather",
-};
-
-export const filterSelectedOptions = Template.bind({});
-filterSelectedOptions.args = {
-  filterSelectedOptions: true,
 };
 
 export const freeSolo = Template.bind({});
@@ -335,25 +246,9 @@ freeSolo.args = {
   freeSolo: true,
 };
 
-export const fullWidth = Template.bind({});
-fullWidth.args = {
-  fullWidth: true,
-};
-
-export const handleHomeEndKeys = Template.bind({});
-handleHomeEndKeys.args = {
-  handleHomeEndKeys: true,
-};
-
 export const includeInputInList = Template.bind({});
 includeInputInList.args = {
   includeInputInList: true,
-};
-
-export const limitTags = Template.bind({});
-limitTags.args = {
-  limitTags: 3,
-  multiple: true,
 };
 
 export const loading = EmptyTemplate.bind({});
@@ -366,20 +261,10 @@ multiple.args = {
   multiple: true,
 };
 
-export const openOnFocus = Template.bind({});
-openOnFocus.args = {
-  openOnFocus: true,
-};
-
 export const readOnly = Template.bind({});
 readOnly.args = {
   readOnly: true,
   value: "The Godfather",
-};
-
-export const selectOnFocus = Template.bind({});
-selectOnFocus.args = {
-  selectOnFocus: true,
 };
 
 export const MaterialSingle = MuiTemplate.bind({});
