0f3244237c1c37803be6732e0536a92759163285
Edbury Enegren <36284167+edburyenegren-okta@users.noreply.github.com>
2023-04-18 11:03:35 -0700

    chore: update stories to match new props

--- packages/odyssey-react-mui/src/theme/components.tsx
+++ packages/odyssey-react-mui/src/theme/components.tsx
@@ -159,6 +159,7 @@ export const components: ThemeOptions["components"] = {
       filterSelectedOptions: false,
       fullWidth: false,
       handleHomeEndKeys: true,
+      includeInputInList: true,
       limitTags: -1,
       openOnFocus: false,
       popupIcon: <ChevronDownIcon />,
