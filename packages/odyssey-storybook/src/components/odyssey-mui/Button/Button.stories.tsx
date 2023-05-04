b4df80aa7239991b8eab3a7a271b9f0a8a56b7e4
Lester Choi <lester.choi@okta.com>
2023-05-04 19:49:10 -0400

    chore: update Button stories and mdx

--- packages/odyssey-storybook/src/components/odyssey-mui/Button/Button.stories.tsx
+++ packages/odyssey-storybook/src/components/odyssey-mui/Button/Button.stories.tsx
@@ -10,24 +10,18 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import type { Meta, StoryFn } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { Button, AddIcon } from "@okta/odyssey-react-mui";
 import type { ButtonProps } from "@okta/odyssey-react-mui";
 import { MuiThemeDecorator } from "../../../../.storybook/components/MuiThemeDecorator";
 
-import ButtonMdx from "./Button.mdx";
 import { userEvent, within } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
 
 const storybookMeta: Meta<ButtonProps> = {
   title: "MUI Components/Button",
   component: Button,
-  parameters: {
-    docs: {
-      page: ButtonMdx,
-    },
-  },
   argTypes: {
     isDisabled: {
       control: "boolean",
@@ -63,77 +57,154 @@ const storybookMeta: Meta<ButtonProps> = {
 
 export default storybookMeta;
 
-const Template: StoryFn<ButtonProps> = (props) => <Button {...props} />;
-
-export const ButtonPrimary = Template.bind({});
-ButtonPrimary.args = {
-  text: "Add crew",
-};
-ButtonPrimary.play = async ({ args, canvasElement }) => {
-  const canvas = within(canvasElement);
-  const button = canvas.getByText("Add crew");
-  await userEvent.hover(button);
-  await userEvent.click(button);
-  await expect(args.onClick).toHaveBeenCalledTimes(1);
-};
-
-export const ButtonSecondary = Template.bind({});
-ButtonSecondary.args = {
-  text: "Add crew",
-  variant: "secondary",
+export const ButtonPrimary: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonDanger = Template.bind({});
-ButtonDanger.args = {
-  text: "Add crew",
-  variant: "danger",
+export const ButtonSecondary: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    variant: "secondary",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonFloating = Template.bind({});
-ButtonFloating.args = {
-  text: "Add crew",
-  variant: "floating",
+export const ButtonDanger: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    variant: "danger",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonSmall = Template.bind({});
-ButtonSmall.args = {
-  text: "Add crew",
-  size: "small",
+export const ButtonFloating: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    variant: "floating",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonMedium = Template.bind({});
-ButtonMedium.args = {
-  text: "Add crew",
-  size: "medium",
+export const ButtonSmall: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    size: "small",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonLarge = Template.bind({});
-ButtonLarge.args = {
-  text: "Add crew",
-  size: "large",
+export const ButtonMedium: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    size: "medium",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonFullWidth = Template.bind({});
-ButtonFullWidth.args = {
-  text: "Add crew",
-  isFullWidth: true,
+export const ButtonLarge: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    size: "large",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonPrimaryDisabled = Template.bind({});
-ButtonPrimaryDisabled.args = {
-  text: "Add crew",
-  isDisabled: true,
+export const ButtonFullWidth: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    isFullWidth: true,
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
 };
 
-export const ButtonWithIcon = Template.bind({});
-ButtonWithIcon.args = {
-  text: "Add crew",
-  startIcon: <AddIcon />,
+export const ButtonPrimaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    isDisabled: true,
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(0);
+  },
 };
 
-export const IconOnly = Template.bind({});
-IconOnly.args = {
-  startIcon: <AddIcon />,
-  text: undefined,
-  tooltipText: "Add crew",
+export const ButtonWithIcon: StoryObj<ButtonProps> = {
+  args: {
+    text: "Add crew",
+    startIcon: <AddIcon />,
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByText("Add crew");
+    await userEvent.hover(button);
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  },
+};
+
+export const IconOnly: StoryObj<ButtonProps> = {
+  args: {
+    startIcon: <AddIcon />,
+    text: undefined, // FIXME
+    tooltipText: "Add crew", // FIXME
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.getByRole("button");
+    await expect(button).toHaveAccessibleName();
+  },
 };
