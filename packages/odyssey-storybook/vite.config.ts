7fe98c2eb549045f0afb22ba06523754d4cbe649
Lester Choi <lester.choi@okta.com>
2023-05-11 14:38:07 -0400

    chore: remove dead code

--- packages/odyssey-storybook/vite.config.ts
+++ packages/odyssey-storybook/vite.config.ts
@@ -17,7 +17,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // assetsInclude: [
-  //   '**/*.md'
-  // ]
 });
