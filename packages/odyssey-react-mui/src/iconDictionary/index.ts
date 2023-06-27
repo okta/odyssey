6ed2aabf9635f0c5dd31b626bdf55bf6e6c46fd9
Edbury Enegren <edbury.enegren@okta.com>
2023-06-27 13:17:10 -0700

    feat(odyssey-react-mui): manually update icon index, update references in styling

--- packages/odyssey-react-mui/src/iconDictionary/index.ts
+++ packages/odyssey-react-mui/src/iconDictionary/index.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2022-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023-present, Okta, Inc. and/or its affiliates. All rights reserved.
  * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
  *
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
@@ -10,167 +10,226 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-// This file used to be generated, but now that's not possible. We need to maintain this file by-hand when icons are added.
-
 import { AddCircleIcon } from "./AddCircle";
-export * from "./AddCircle";
-
 import { AddIcon } from "./Add";
-export * from "./Add";
-
-import { AlertCircleFilledIcon } from "./AlertCircleFilled";
-export * from "./AlertCircleFilled";
-
-import { AlertCircleIcon } from "./AlertCircle";
-export * from "./AlertCircle";
-
-import { AlertTriangleFilledIcon } from "./AlertTriangleFilled";
-export * from "./AlertTriangleFilled";
-
-import { AnchorIcon } from "./Anchor";
-export * from "./Anchor";
-
+import { AppsIcon } from "./Apps";
 import { ArrowDownIcon } from "./ArrowDown";
-export * from "./ArrowDown";
-
 import { ArrowLeftIcon } from "./ArrowLeft";
-export * from "./ArrowLeft";
-
+import { ArrowLowerLeftIcon } from "./ArrowLowerLeft";
+import { ArrowLowerRightIcon } from "./ArrowLowerRight";
 import { ArrowRightIcon } from "./ArrowRight";
-export * from "./ArrowRight";
-
 import { ArrowUpDownIcon } from "./ArrowUpDown";
-export * from "./ArrowUpDown";
-
 import { ArrowUpIcon } from "./ArrowUp";
-export * from "./ArrowUp";
-
+import { ArrowUpperLeftIcon } from "./ArrowUpperLeft";
+import { ArrowUpperRightIcon } from "./ArrowUpperRight";
+import { BugIcon } from "./Bug";
 import { CalendarIcon } from "./Calendar";
-export * from "./Calendar";
-
+import { CallIcon } from "./Call";
+import { ChatIcon } from "./Chat";
 import { CheckCircleFilledIcon } from "./CheckCircleFilled";
-export * from "./CheckCircleFilled";
-
 import { CheckIcon } from "./Check";
-export * from "./Check";
-
 import { ChevronDownIcon } from "./ChevronDown";
-export * from "./ChevronDown";
-
+import { ChevronLeftIcon } from "./ChevronLeft";
+import { ChevronRightIcon } from "./ChevronRight";
 import { ChevronUpIcon } from "./ChevronUp";
-export * from "./ChevronUp";
-
+import { ClockIcon } from "./Clock";
 import { CloseCircleFilledIcon } from "./CloseCircleFilled";
-export * from "./CloseCircleFilled";
-
 import { CloseIcon } from "./Close";
-export * from "./Close";
-
+import { CollapseLeftIcon } from "./CollapseLeft";
+import { CollapseRightIcon } from "./CollapseRight";
 import { CopyIcon } from "./Copy";
-export * from "./Copy";
-
+import { DangerDiamondFilledIcon } from "./DangerDiamondFilled";
+import { DangerDiamondIcon } from "./DangerDiamond";
 import { DeleteIcon } from "./Delete";
-export * from "./Delete";
-
+import { DenyIcon } from "./Deny";
+import { DevicesIcon } from "./Devices";
+import { DirectoryIcon } from "./Directory";
+import { DocumentationIcon } from "./Documentation";
 import { DownloadIcon } from "./Download";
-export * from "./Download";
-
-import { DragHandleIcon } from "./DragHandle";
-export * from "./DragHandle";
-
 import { EditIcon } from "./Edit";
-export * from "./Edit";
-
+import { ExpandLeftIcon } from "./ExpandLeft";
+import { ExpandRightIcon } from "./ExpandRight";
 import { ExternalLinkIcon } from "./ExternalLink";
-export * from "./ExternalLink";
-
 import { EyeOffIcon } from "./EyeOff";
-export * from "./EyeOff";
-
 import { EyeIcon } from "./Eye";
-export * from "./Eye";
-
 import { FilterIcon } from "./Filter";
-export * from "./Filter";
-
+import { FolderIcon } from "./Folder";
 import { GlobeIcon } from "./Globe";
-export * from "./Globe";
-
+import { GridIcon } from "./Grid";
+import { GroupIcon } from "./Group";
 import { HomeIcon } from "./Home";
-export * from "./Home";
-
 import { InformationCircleFilledIcon } from "./InformationCircleFilled";
-export * from "./InformationCircleFilled";
-
 import { InformationCircleIcon } from "./InformationCircle";
-export * from "./InformationCircle";
-
+import { LinkIcon } from "./Link";
+import { ListIcon } from "./List";
+import { LockIcon } from "./Lock";
 import { NotificationIcon } from "./Notification";
-export * from "./Notification";
-
+import { OverflowVertical1Icon } from "./OverflowVertical1";
 import { OverflowVerticalIcon } from "./OverflowVertical";
-export * from "./OverflowVertical";
-
+import { PauseIcon } from "./Pause";
 import { QuestionCircleFilledIcon } from "./QuestionCircleFilled";
-export * from "./QuestionCircleFilled";
-
 import { QuestionCircleIcon } from "./QuestionCircle";
-export * from "./QuestionCircle";
-
+import { RefreshIcon } from "./Refresh";
+import { ResetIcon } from "./Reset";
+import { ResumeIcon } from "./Resume";
 import { SearchIcon } from "./Search";
-export * from "./Search";
-
+import { ServerIcon } from "./Server";
 import { SettingsIcon } from "./Settings";
-export * from "./Settings";
-
 import { SubtractIcon } from "./Subtract";
-export * from "./Subtract";
-
-import { UserGroupIcon } from "./UserGroup";
-export * from "./UserGroup";
-
+import { SyncIcon } from "./Sync";
+import { UnlockIcon } from "./Unlock";
+import { UploadIcon } from "./Upload";
 import { UserIcon } from "./User";
+import { VideoIcon } from "./Video";
+import { WarningFilledIcon } from "./WarningFilled";
+import { WarningIcon } from "./Warning";
+
+export * from "./AddCircle";
+export * from "./Add";
+export * from "./Apps";
+export * from "./ArrowDown";
+export * from "./ArrowLeft";
+export * from "./ArrowLowerLeft";
+export * from "./ArrowLowerRight";
+export * from "./ArrowRight";
+export * from "./ArrowUpDown";
+export * from "./ArrowUp";
+export * from "./ArrowUpperLeft";
+export * from "./ArrowUpperRight";
+export * from "./Bug";
+export * from "./Calendar";
+export * from "./Call";
+export * from "./Chat";
+export * from "./CheckCircleFilled";
+export * from "./Check";
+export * from "./ChevronDown";
+export * from "./ChevronLeft";
+export * from "./ChevronRight";
+export * from "./ChevronUp";
+export * from "./Clock";
+export * from "./CloseCircleFilled";
+export * from "./Close";
+export * from "./CollapseLeft";
+export * from "./CollapseRight";
+export * from "./Copy";
+export * from "./DangerDiamondFilled";
+export * from "./DangerDiamond";
+export * from "./Delete";
+export * from "./Deny";
+export * from "./Devices";
+export * from "./Directory";
+export * from "./Documentation";
+export * from "./Download";
+export * from "./Edit";
+export * from "./ExpandLeft";
+export * from "./ExpandRight";
+export * from "./ExternalLink";
+export * from "./EyeOff";
+export * from "./Eye";
+export * from "./Filter";
+export * from "./Folder";
+export * from "./Globe";
+export * from "./Grid";
+export * from "./Group";
+export * from "./Home";
+export * from "./InformationCircleFilled";
+export * from "./InformationCircle";
+export * from "./Link";
+export * from "./List";
+export * from "./Lock";
+export * from "./Notification";
+export * from "./OverflowVertical1";
+export * from "./OverflowVertical";
+export * from "./Pause";
+export * from "./QuestionCircleFilled";
+export * from "./QuestionCircle";
+export * from "./Refresh";
+export * from "./Reset";
+export * from "./Resume";
+export * from "./Search";
+export * from "./Server";
+export * from "./Settings";
+export * from "./Subtract";
+export * from "./Sync";
+export * from "./Unlock";
+export * from "./Upload";
 export * from "./User";
+export * from "./Video";
+export * from "./WarningFilled";
+export * from "./Warning";
 
 export const iconDictionary = {
   "add-circle": AddCircleIcon,
   add: AddIcon,
-  "alert-circle-filled": AlertCircleFilledIcon,
-  "alert-circle": AlertCircleIcon,
-  "alert-triangle-filled": AlertTriangleFilledIcon,
-  anchor: AnchorIcon,
+  apps: AppsIcon,
   "arrow-down": ArrowDownIcon,
   "arrow-left": ArrowLeftIcon,
+  "arrow-lower-left": ArrowLowerLeftIcon,
+  "arrow-lower-right": ArrowLowerRightIcon,
   "arrow-right": ArrowRightIcon,
   "arrow-up-down": ArrowUpDownIcon,
   "arrow-up": ArrowUpIcon,
+  "arrow-upper-left": ArrowUpperLeftIcon,
+  "arrow-upper-right": ArrowUpperRightIcon,
+  bug: BugIcon,
   calendar: CalendarIcon,
+  call: CallIcon,
+  chat: ChatIcon,
   "check-circle-filled": CheckCircleFilledIcon,
   check: CheckIcon,
   "chevron-down": ChevronDownIcon,
+  "chevron-left": ChevronLeftIcon,
+  "chevron-right": ChevronRightIcon,
   "chevron-up": ChevronUpIcon,
+  clock: ClockIcon,
   "close-circle-filled": CloseCircleFilledIcon,
   close: CloseIcon,
+  "collapse-left": CollapseLeftIcon,
+  "collapse-right": CollapseRightIcon,
   copy: CopyIcon,
+  "danger-diamond-filled": DangerDiamondFilledIcon,
+  "danger-diamond": DangerDiamondIcon,
   delete: DeleteIcon,
+  deny: DenyIcon,
+  devices: DevicesIcon,
+  directory: DirectoryIcon,
+  documentation: DocumentationIcon,
   download: DownloadIcon,
-  "drag-handle": DragHandleIcon,
   edit: EditIcon,
+  "expand-left": ExpandLeftIcon,
+  "expand-right": ExpandRightIcon,
   "external-link": ExternalLinkIcon,
   "eye-off": EyeOffIcon,
   eye: EyeIcon,
   filter: FilterIcon,
+  folder: FolderIcon,
   globe: GlobeIcon,
+  grid: GridIcon,
+  group: GroupIcon,
   home: HomeIcon,
   "information-circle-filled": InformationCircleFilledIcon,
   "information-circle": InformationCircleIcon,
+  link: LinkIcon,
+  list: ListIcon,
+  lock: LockIcon,
   notification: NotificationIcon,
+  "overflow-vertical1": OverflowVertical1Icon,
   "overflow-vertical": OverflowVerticalIcon,
+  pause: PauseIcon,
   "question-circle-filled": QuestionCircleFilledIcon,
   "question-circle": QuestionCircleIcon,
+  refresh: RefreshIcon,
+  reset: ResetIcon,
+  resume: ResumeIcon,
   search: SearchIcon,
+  server: ServerIcon,
   settings: SettingsIcon,
   subtract: SubtractIcon,
-  "user-group": UserGroupIcon,
+  sync: SyncIcon,
+  unlock: UnlockIcon,
+  upload: UploadIcon,
   user: UserIcon,
+  video: VideoIcon,
+  "warning-filled": WarningFilledIcon,
+  warning: WarningIcon,
 };
