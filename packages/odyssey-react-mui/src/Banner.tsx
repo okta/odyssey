db10951fc0e164452b8be892f7f7495646061301
jordankoschei-okta <jordan.koschei@okta.com>
2023-07-07 09:18:20 -0400

    refactor: fix import relativity

--- packages/odyssey-react-mui/src/Banner.tsx
+++ packages/odyssey-react-mui/src/Banner.tsx
@@ -10,14 +10,9 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {
-  Alert,
-  AlertTitle,
-  AlertProps,
-  Link,
-  ScreenReaderText,
-  AlertColor,
-} from "./";
+import { Alert, AlertColor, AlertTitle, AlertProps } from "@mui/material";
+import { Link } from "./Link";
+import { ScreenReaderText } from "./ScreenReaderText";
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
 
