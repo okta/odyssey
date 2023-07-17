ccb5df22dc1118e99a1327468e0bf06f4dfaf07f
Kevin Ghadyani <kevin.ghadyani@okta.com>
2023-07-17 16:33:42 -0500

    feat: merged labs with mui

--- packages/odyssey-storybook/src/components/odyssey-labs/DatePicker/DatePicker.stories.tsx
+++ packages/odyssey-storybook/src/components/odyssey-labs/DatePicker/DatePicker.stories.tsx
@@ -19,7 +19,7 @@ import {
   DatePickerProps,
   datePickerTheme,
   LocalizationProvider,
-} from "@okta/odyssey-react-labs";
+} from "@okta/odyssey-react-mui/labs";
 
 import { MuiThemeDecorator } from "../../../../.storybook/components/MuiThemeDecorator";
 
