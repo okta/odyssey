131c4073a67f6fe8cf6799f1068735231e70263e
Ganesh Somasundaram <ganesh.somasundaram@okta.com>
2023-08-07 16:47:47 -0400

    feat: add LTR-RTL button to support rtl languages

--- packages/odyssey-storybook/.storybook/main.ts
+++ packages/odyssey-storybook/.storybook/main.ts
@@ -26,6 +26,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-mdx-gfm"),
+    getAbsolutePath("storybook-addon-rtl-direction"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
