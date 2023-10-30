35d82350e3109f8ec7b728fcc7ecbafe5ab4949b
bryancunningham-okta <148223016+bryancunningham-okta@users.noreply.github.com>
2023-10-30 15:00:03 -0400

    Add locale picker to Storybook toolbar (#2023)
    * feat(odyssey-react-mui): add locale picker to Storybook toolbar
    
    * feat(odyssey-react-mui): remove console.log left in error and implicitly return from provider

--- packages/odyssey-storybook/.storybook/preview.ts
+++ packages/odyssey-storybook/.storybook/preview.ts
@@ -2,6 +2,45 @@ import type { Preview } from "@storybook/react";
 
 export const globalTypes = {
   rtlDirection: "ltr",
+  locale: {
+    name: "Locale",
+    description: "Internationalization locale",
+    toolbar: {
+      icon: "globe",
+      items: [
+        { value: "cs", title: "Czech" },
+        { value: "da", title: "Danish" },
+        { value: "de", title: "Deutsch" },
+        { value: "el", title: "Greek" },
+        { value: "en", title: "English" },
+        { value: "es", title: "Spanish" },
+        { value: "fi", title: "Finnish" },
+        { value: "fr", title: "French" },
+        { value: "hu", title: "Hungarian" },
+        { value: "id", title: "Indonesian" },
+        { value: "it", title: "Italian" },
+        { value: "ja", title: "Japanese" },
+        { value: "ko", title: "Korean" },
+        { value: "ms", title: "Malaysian" },
+        { value: "nb", title: "Norwegian" },
+        { value: "nl_NL", title: "Dutch" },
+        { value: "ok_PL", title: "(Test Language) Pseudo-loc" },
+        { value: "ok_SK", title: "(Test Language) Show Keys" },
+        { value: "pl", title: "Polish" },
+        { value: "pt_BR", title: "Portuguese (Brazil)" },
+        { value: "ro", title: "Romanian" },
+        { value: "ru", title: "Russian" },
+        { value: "sv", title: "Swedish" },
+        { value: "th", title: "Thai" },
+        { value: "tr", title: "Turkish" },
+        { value: "uk", title: "Ukrainian" },
+        { value: "vi", title: "Vietnamese" },
+        { value: "zh_CN", title: "Chinese (PRC)" },
+        { value: "zh_TW", title: "Chinese" },
+      ],
+      showName: true,
+    },
+  },
 };
 
 const preview: Preview = {
