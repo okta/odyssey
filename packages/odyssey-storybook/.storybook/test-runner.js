250fcd620d1dfb53cd95b639395c90dd8307a372
benlister-okta <ben.lister@okta.com>
2024-02-21 08:55:46 -0800

    fix: undo unrelated test-runner change

--- packages/odyssey-storybook/.storybook/test-runner.js
+++ packages/odyssey-storybook/.storybook/test-runner.js
@@ -13,7 +13,7 @@ module.exports = {
    * The page argument is the Playwright's page object for the story.
    * The context argument is a Storybook object containing the story's id, title, and name.
    */
-  async preVisit(page, context) {
+  async preRender(page, context) {
     await injectAxe(page);
   },
 
