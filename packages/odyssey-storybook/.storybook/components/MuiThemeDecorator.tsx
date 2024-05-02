2a8f9a2acb5dc2df3356ce1d81f3fbb3e1c938a0
Trevor Ing <trevor.ing@okta.com>
2024-05-02 16:55:14 -0400

    fix: remove unncessary MUI ScopedCssBaseline

--- packages/odyssey-storybook/.storybook/components/MuiThemeDecorator.tsx
+++ packages/odyssey-storybook/.storybook/components/MuiThemeDecorator.tsx
@@ -3,7 +3,6 @@ import {
   CssBaseline,
   OdysseyProvider,
 } from "@okta/odyssey-react-mui";
-// import { ScopedCssBaseline } from "@mui/material";
 import { ThemeProvider as StorybookThemeProvider } from "@storybook/theming";
 import type { Decorator } from "@storybook/react";
 import * as odysseyTokens from "@okta/odyssey-design-tokens";
@@ -25,9 +24,7 @@ export const MuiThemeDecorator: Decorator = (Story, context) => {
       <StorybookThemeProvider theme={odysseyTheme}>
         <CssBaseline />
         <div style={styles} lang={locale}>
-          {/* <ScopedCssBaseline> */}
           <Story />
-          {/* </ScopedCssBaseline> */}
         </div>
       </StorybookThemeProvider>
     </OdysseyProvider>
