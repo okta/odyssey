8a5782f9ffaa43a82329dcf5074732242c9a204a
Glen Fannin <glen.fannin@okta.com>
2024-05-15 12:07:51 -0400

    fix: fixes publish script to use commit sha

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,27 +17,30 @@ echo "Artifactory URL: $PUBLISH_REGISTRY \n"
 
 cd $OKTA_HOME/$REPO
 # yarn run lerna-version --yes
-# npm config set @okta:registry ${PUBLISH_REGISTRY}
-# PACKAGES=$(echo odyssey-{design-tokens,babel-preset,babel-loader,react-mui} browserslist-config-odyssey)
-# CURRENT_VERSION=$(< lerna.json jq -r '.version')
-# TAGGED_VERSION=$CURRENT_VERSION-$PUBLISH_SHA
+npm config set @okta:registry ${PUBLISH_REGISTRY}
+PACKAGES=$(echo odyssey-{design-tokens,babel-preset,babel-loader,react-mui} browserslist-config-odyssey)
+CURRENT_VERSION=$(< lerna.json jq -r '.version')
+TAGGED_VERSION=$CURRENT_VERSION-$PUBLISH_SHA
 
-# function lerna_publish() {
-# #   MY_CMD="yarn lerna-publish --loglevel silly --dist-tag \"${PUBLISH_SHA}\" --registry \"${PUBLISH_REGISTRY}\" --yes --no-push --no-git-tag-version"
-#   MY_CMD="yarn run lerna publish from-package --ignore-changes --no-push --no-git-tag-version --no-verify-access --registry \"${PUBLISH_REGISTRY}\" --yes"
-#   echo "Running ${MY_CMD}"
-#   ${MY_CMD}
-# }
+function lerna_publish() {
+#   MY_CMD="yarn lerna-publish --loglevel silly --dist-tag \"${PUBLISH_SHA}\" --registry \"${PUBLISH_REGISTRY}\" --yes --no-push --no-git-tag-version"
+  MY_CMD="yarn run lerna publish from-package --ignore-changes --no-push --no-git-tag-version --no-verify-access --registry \"${PUBLISH_REGISTRY}\" --yes"
+  echo "Running ${MY_CMD}"
+  ${MY_CMD}
+}
 
-# echo "Publishing to artifactory"
-# git status
-# git update-index --assume-unchanged scripts/publish.sh
-# git update-index --assume-unchanged yarn.lock
-# if ! lerna_publish; then
-#   echo "ERROR: Lerna Publish has failed."
-#   exit $PUBLISH_ARTIFACTORY_FAILURE
-# else
-#   echo "Publish successful. Sending promotion message"
-# fi
+lerna_json_contents="$(jq '.version = "'$TAGGED_VERSION'"' $OKTA_HOME/$REPO/lerna.json)" && \
+echo -E "${lerna_json_contents}" > $OKTA_HOME/$REPO/lerna.json
+
+echo "Publishing to artifactory"
+git status
+git update-index --assume-unchanged scripts/publish.sh
+git update-index --assume-unchanged yarn.lock
+if ! lerna_publish; then
+  echo "ERROR: Lerna Publish has failed."
+  exit $PUBLISH_ARTIFACTORY_FAILURE
+else
+  echo "Publish successful. Sending promotion message"
+fi
 
 exit $SUCCESS
