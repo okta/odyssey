3c5d5e585510a0fea90dc29636c77228c6864dd9
Glen Fannin <glen.fannin@okta.com>
2024-05-15 12:58:38 -0400

    fix: update temp version for each package

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,14 +17,21 @@ function lerna_publish() {
   ${MY_CMD}
 }
 
-lerna_json_contents="$(jq '.version = "'$TAGGED_VERSION'"' $OKTA_HOME/$REPO/lerna.json)" && \
-echo -E "${lerna_json_contents}" > $OKTA_HOME/$REPO/lerna.json
+FILES_TO_UPDATE_VERSION="lerna.json packages/odyssey-design-tokens/package.json packages/odyssey-babel-preset/package.json packages/odyssey-babel-loader/package.json packages/odyssey-react-mui/package.json packages/browserslist-config-odyssey/package.json"
+for PATH_AND_FILE in $FILES_TO_UPDATE_VERSION; do
+  FULL_PATH="$OKTA_HOME/$REPO/$PATH_AND_FILE"
+  json_contents="$(jq '.version = "'$TAGGED_VERSION'"' $FULL_PATH)" && \
+  echo -E "${json_contents}" > $FULL_PATH
+  git update-index --assume-unchanged $FULL_PATH
+done
+# lerna_json_contents="$(jq '.version = "'$TAGGED_VERSION'"' $OKTA_HOME/$REPO/lerna.json)" && \
+# echo -E "${lerna_json_contents}" > $OKTA_HOME/$REPO/lerna.json
 
 echo "Publishing to artifactory"
 # assume all files unchanged for lerna publish
 git update-index --assume-unchanged scripts/publish.sh
 git update-index --assume-unchanged yarn.lock
-git update-index --assume-unchanged lerna.json
+# git update-index --assume-unchanged lerna.json
 if ! lerna_publish; then
   echo "ERROR: Lerna Publish has failed."
   exit $PUBLISH_ARTIFACTORY_FAILURE
