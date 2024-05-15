35f9d0223c4cb8fd80f761add821d38b73591591
Glen Fannin <glen.fannin@okta.com>
2024-05-15 19:44:56 -0400

    fix: install playwright deps

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -7,25 +7,10 @@ source $OKTA_HOME/$REPO/scripts/setup.sh
 
 cd $OKTA_HOME/$REPO
 
-apt-get install libnss3\
-  libnspr4\
-  libdbus-1-3\
-  libatk1.0-0\
-  libatk-bridge2.0-0\
-  libcups2\
-  libdrm2\
-  libatspi2.0-0\
-  libx11-6\
-  libxcomposite1\
-  libxdamage1\
-  libxext6\
-  libxfixes3\
-  libxrandr2\
-  libgbm1\
-  libxcb1\
-  libpango-1.0-0\
-  libcairo2\
-  libasound2
+if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
+  echo "playwright dependencies failed to install"
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+fi
 
 if ! yarn workspace @okta/odyssey-storybook coverage; then
   echo "code coverage failed! Exiting..."
