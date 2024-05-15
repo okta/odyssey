27e0a76ef87da757d94acafad8ddc5d8810d16ef
Glen Fannin <glen.fannin@okta.com>
2024-05-15 19:29:25 -0400

    fix: installing playwright deps for CI run

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -7,6 +7,11 @@ source $OKTA_HOME/$REPO/scripts/setup.sh
 
 cd $OKTA_HOME/$REPO
 
+if ! yarn playwright install-deps; then
+  echo "failed to install playwright dependencies for coverage"
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+fi
+
 if ! yarn workspace @okta/odyssey-storybook coverage; then
   echo "code coverage failed! Exiting..."
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
