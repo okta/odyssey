b84c71c561f84c6bcfa33a7a9fa6edfe9682a1bd
Glen Fannin <glen.fannin@okta.com>
2024-05-15 20:02:19 -0400

    fix: install apt-get

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -7,6 +7,9 @@ source $OKTA_HOME/$REPO/scripts/setup.sh
 
 cd $OKTA_HOME/$REPO
 
+# install apt-get
+sudo dpkg -i apt.deb
+
 if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
   echo "playwright dependencies failed to install"
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
