407647e8795fa691fa9220bd215add710eb3dcb1
Glen Fannin <glen.fannin@okta.com>
2024-05-16 14:49:41 -0400

    fix: update script for dockerfile

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CHROME_HEADLESS=true
-setup_service google-chrome-stable 83.0.4103.61-1
+# export CHROME_HEADLESS=true
+# setup_service google-chrome-stable 83.0.4103.61-1
 
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
@@ -9,19 +9,21 @@ cd $OKTA_HOME/$REPO
 
 setup_service docker
 
-docker pull mcr.microsoft.com/playwright:v1.44.0-jammy
+# docker pull mcr.microsoft.com/playwright:v1.44.0-jammy
 
-docker run -it --rm --ipc=host mcr.microsoft.com/playwright:v1.44.0-jammy /bin/bash
+# docker run -it --rm --ipc=host mcr.microsoft.com/playwright:v1.44.0-jammy /bin/bash
+
+docker run --rm -it $(docker build -q $OKTA_HOME/$REPO/packages/odyssey-storybook)
 
 # install apt-get
 # dpkg -i apt.deb
 # wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
-if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
-  echo "playwright dependencies failed to install"
-  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
-fi
+# if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
+#   echo "playwright dependencies failed to install"
+#   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+# fi
 
-# if ! yarn workspace @okta/odyssey-storybook coverage; then
+# if ! yarn workspace @okta/odyssey-storybook ci:coverage; then
 #   echo "code coverage failed! Exiting..."
 #   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 # fi
--- scripts/interaction-test.sh
+++ scripts/interaction-test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-export CHROME_HEADLESS=true
-setup_service google-chrome-stable 83.0.4103.61-1
+# export CHROME_HEADLESS=true
+# setup_service google-chrome-stable 83.0.4103.61-1
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+# source $OKTA_HOME/$REPO/scripts/setup.sh
 
 cd $OKTA_HOME/$REPO
 
-if ! yarn workspace @okta/odyssey-storybook dev:interactionTest; then
-  echo "interaction tests failed! Exiting..."
-  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
-fi
+setup_service docker
+
+docker run --rm -it $(docker build -q $OKTA_HOME/$REPO/packages/odyssey-storybook)
+
+# if ! yarn workspace @okta/odyssey-storybook dev:interactionTest; then
+#   echo "interaction tests failed! Exiting..."
+#   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+# fi
 
 exit $PUBLISH_TYPE_AND_RESULT_DIR;
