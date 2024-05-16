e6541c55022a0ae854927121b57618c501c07aee
Glen Fannin <glen.fannin@okta.com>
2024-05-16 10:45:57 -0400

    fix: adds docker setup for deps install

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -9,8 +9,9 @@ cd $OKTA_HOME/$REPO
 
 # install apt-get
 # dpkg -i apt.deb
-wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
+# wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
 
+setup_service docker
 if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
   echo "playwright dependencies failed to install"
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
