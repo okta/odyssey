a446e5bacb1159a6e38e37c649faf241c451c9c9
Glen Fannin <glen.fannin@okta.com>
2024-05-16 17:02:11 -0400

    fix: fixing interaction test script

--- scripts/interaction-test.sh
+++ scripts/interaction-test.sh
@@ -9,7 +9,7 @@ cd $OKTA_HOME/$REPO
 
 setup_service docker
 
-docker run --progress=plain --rm -it $(docker build $OKTA_HOME/$REPO/packages/odyssey-storybook)
+docker run --rm -it $(docker build .)
 
 # if ! yarn workspace @okta/odyssey-storybook dev:interactionTest; then
 #   echo "interaction tests failed! Exiting..."
