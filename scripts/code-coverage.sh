a60e991266f83badc849f5e46deba70fbc673827
Glen Fannin <glen.fannin@okta.com>
2024-05-16 10:59:08 -0400

    fix: re-arranging dpkg command

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -7,11 +7,11 @@ source $OKTA_HOME/$REPO/scripts/setup.sh
 
 cd $OKTA_HOME/$REPO
 
+setup_service docker
+
 # install apt-get
 dpkg -i apt.deb
 # wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
-
-setup_service docker
 if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
   echo "playwright dependencies failed to install"
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
