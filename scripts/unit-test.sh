097998539b9572271e48d80c7fc37fe445a5dee0
Glen Fannin <glen.fannin@okta.com>
2024-05-20 12:13:40 -0400

    fix: fixes unit-test suite

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -4,6 +4,11 @@ source $OKTA_HOME/$REPO/scripts/setup.sh
 
 cd $OKTA_HOME/$REPO
 
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/unit"
+echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
+echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+
 if ! yarn test; then
   echo "lerna tests failed! Exiting..."
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
