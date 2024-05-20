1ad142295369c091447912377d733cf4333fba23
Glen Fannin <glen.fannin@okta.com>
2024-05-20 11:33:15 -0400

    fix: fix to interaction test bacon suite

--- scripts/interaction-test.sh
+++ scripts/interaction-test.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+source $OKTA_HOME/$REPO/scripts/setup.sh
+
 cd $OKTA_HOME/$REPO
 
-setup_service docker
+# setup_service docker
 
-docker build -t storybook-interaction-tests . && docker run --rm storybook-interaction-tests
+# docker build -t storybook-interaction-tests . && docker run --rm storybook-interaction-tests
 
-exit $PUBLISH_TYPE_AND_RESULT_DIR;
+# exit $PUBLISH_TYPE_AND_RESULT_DIR;
+
+# Bacon required config
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${OKTA_HOME}/${REPO}/build2/reports/playwright"
+echo ${TEST_SUITE_TYPE} > "${TEST_SUITE_TYPE_FILE}"
+echo "${TEST_RESULT_FILE_DIR}" > "${TEST_RESULT_FILE_DIR_FILE}"
+
+if ! yarn workspace @okta/odyssey-storybook playwright install --with-deps chromium; then
+  echo "Failed to install Playwright and its dependencies!"
+  report_results FAILURE publish_type_and_result_dir_but_always_fail
+  exit "$BUILD_FAILURE"
+fi
+
+if ! yarn workspace @okta/odyssey-storybook ci:dev:interactionTest; then
+  echo "Playwright test failure!"
+  report_results FAILURE publish_type_and_result_dir_but_always_fail
+  exit "$BUILD_FAILURE"
+fi
+
+echo "Playwright tests passed!"
+report_results SUCCESS publish_type_and_result_dir_but_succeed_if_no_results
