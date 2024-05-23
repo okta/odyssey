33cf9d22a33cf1a07d8f29c9b454a4d11af8c213
bryancunningham-okta <bryan.cunningham@okta.com>
2024-05-23 14:31:04 -0400

    feat(odyssey-react-mui): add internal custom autocomplete

--- packages/odyssey-react-mui/src/CustomizableAutocomplete.tsx
+++ packages/odyssey-react-mui/src/CustomizableAutocomplete.tsx
@@ -38,176 +38,81 @@ import _AutoSizer, {
   Size as AutoSizerSize,
 } from "react-virtualized-auto-sizer";
 
+import { AutocompleteProps } from "./Autocomplete";
+
 // This is required to get around a react-types issue for "AutoSizer is not a valid JSX element."
 // @see https://github.com/bvaughn/react-virtualized/issues/1739#issuecomment-1291444246
 const AutoSizer = _AutoSizer as unknown as FC<AutoSizerProps>;
 
 import { Field } from "./Field";
-import { FieldComponentProps } from "./FieldComponentProps";
-import type { HtmlProps } from "./HtmlProps";
 import {
   ComponentControlledState,
   useInputValues,
   getControlState,
 } from "./inputUtils";
 
-export type AutocompleteProps<
+type SetItemSize = (index: number, size: number) => void;
+
+const Row = ({
+  data,
+  index,
+  setItemSize,
+  style,
+}: ListChildComponentProps & { setItemSize: SetItemSize }) => {
+  const rowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (rowRef.current) {
+      /**
+       * Checking for child height to workaround a bug where the clientHeight of the row isn't updated correctly
+       * @see here if you need to know more: https://github.com/bvaughn/react-window/issues/582#issuecomment-1883074908
+       */
+      const firstChild = rowRef.current.firstElementChild;
+      if (firstChild) {
+        const height = firstChild.clientHeight;
+        setItemSize(index, height);
+      }
+    }
+  }, [index, rowRef, setItemSize]);
+
+  /**
+   * react-window calculates the absolute positions of the list items, via an inline style, so
+   * we need to add it to each list item that is being rendered in the viewable list window.
+   * @see here if you need to know more: https://github.com/bvaughn/react-window?tab=readme-ov-file#why-is-my-list-blank-when-i-scroll
+   */
+  return (
+    <div key={`${index}-${style}}`} style={style} ref={rowRef}>
+      {data[index]}
+    </div>
+  );
+};
+
+export type CustomizableAutocompleteProps<
   OptionType,
   HasMultipleChoices extends boolean | undefined,
   IsCustomValueAllowed extends boolean | undefined,
-> = {
-  /**
-   * The default value. Use when the component is not controlled.
-   */
-  defaultValue?: UseAutocompleteProps<
+> = AutocompleteProps<OptionType, HasMultipleChoices, IsCustomValueAllowed> & {
+  renderOption?: MuiAutocompleteProps<
     OptionType,
     HasMultipleChoices,
     undefined,
     IsCustomValueAllowed
-  >["defaultValue"];
-  /**
-   * Enables multiple choice selection
-   */
-  hasMultipleChoices?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["multiple"];
-  /**
-   * The value for the input
-   */
-  inputValue?: UseAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["inputValue"];
-  /**
-   * Allows the input of custom values
-   */
-  isCustomValueAllowed?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["freeSolo"];
-  /**
-   * Disables the Autocomplete input
-   */
-  isDisabled?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["disabled"];
-  /**
-   * Displays a loading indicator
-   */
-  isLoading?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["loading"];
-  /**
-   * Makes the Autocomplete input read-only
-   */
-  isReadOnly?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["readOnly"];
-  /**
-   * The label text for the autocomplete input
-   */
-  label: string;
-  /**
-   * Callback fired when the autocomplete loses focus.
-   */
-  onBlur?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["onBlur"];
-  /**
-   * Callback fired when a selection is made.
-   */
-  onChange?: UseAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["onChange"];
-  /**
-   * Callback fired when the textbox receives typed characters.
-   */
-  onInputChange?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["onInputChange"];
-  /**
-   * Callback fired when the autocomplete gains focus.
-   */
-  onFocus?: MuiAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["onFocus"];
-  /**
-   * The options for the Autocomplete input
-   */
-  options: ReadonlyArray<OptionType>;
-  /**
-   * The value of the Autocomplete input
-   */
-  value?: UseAutocompleteProps<
-    OptionType,
-    HasMultipleChoices,
-    undefined,
-    IsCustomValueAllowed
-  >["value"];
+  >["renderOption"];
 
-  /**
-   * Used to determine if the option represents the given value. Uses strict equality by default if none provided.
-   * Both arguments need to be handled, an option can only match with one value.
-   * option: the option to test
-   * value: the value to test against
-   *
-   * You will need to implement this function if your `option` items are objects.
-   */
-  getIsOptionEqualToValue?: (option: OptionType, value: OptionType) => boolean;
-
-  /**
-   * If this component is required to display a virtualized list of options,
-   * then this value needs to be set to true.
-   * It is recommended if you're options are on the order of 10's of hundreds or more.
-   */
-  isVirtualized?: boolean;
-} & Pick<
-  FieldComponentProps,
-  | "errorMessage"
-  | "errorMessageList"
-  | "hint"
-  | "HintLinkComponent"
-  | "id"
-  | "isFullWidth"
-  | "isOptional"
-  | "name"
-> &
-  Pick<HtmlProps, "ariaDescribedBy" | "testId" | "translate">;
+  renderTags?: MuiAutocompleteProps<
+    OptionType,
+    HasMultipleChoices,
+    undefined,
+    IsCustomValueAllowed
+  >["renderTags"];
+};
 
 const ListboxContainer = styled.div`
   width: 100%;
   height: 100%;
 `;
 
-const Autocomplete = <
+const CustomizableAutocomplete = <
   OptionType,
   HasMultipleChoices extends boolean | undefined,
   IsCustomValueAllowed extends boolean | undefined,
@@ -216,6 +121,8 @@ const Autocomplete = <
   defaultValue,
   errorMessage,
   errorMessageList,
+  getIsOptionEqualToValue,
+  getOptionLabel,
   hasMultipleChoices,
   id: idOverride,
   inputValue,
@@ -235,11 +142,16 @@ const Autocomplete = <
   onInputChange: onInputChangeProp,
   onFocus,
   options,
+  renderOption,
+  renderTags,
   value,
-  getIsOptionEqualToValue,
   testId,
   translate,
-}: AutocompleteProps<OptionType, HasMultipleChoices, IsCustomValueAllowed>) => {
+}: CustomizableAutocompleteProps<
+  OptionType,
+  HasMultipleChoices,
+  IsCustomValueAllowed
+>) => {
   const controlledStateRef = useRef(
     getControlState({
       controlledValue: value,
@@ -340,37 +252,12 @@ const Autocomplete = <
     ],
   );
 
-  const renderVirtualizedRow = ({
-    data,
-    index,
-    style,
-  }: ListChildComponentProps) => {
-    const baseOption = data[index];
-    /**
-     * react-window calculates the absolute positions of the list items, via an inline style, so
-     * we need to add it to each list item that is being rendered in the viewable list window.
-     * See here if you need to know more: https://github.com/bvaughn/react-window?tab=readme-ov-file#why-is-my-list-blank-when-i-scroll
-     */
-    const optionItem = { ...baseOption, props: { ...baseOption.props, style } };
-    return optionItem;
-  };
-
   const OuterListboxContext = createContext({});
 
   const OuterListboxElementType = forwardRef<HTMLDivElement>((props, ref) => {
     const outerProps = useContext(OuterListboxContext);
     return <div ref={ref} {...props} {...outerProps} />;
   });
-
-  function useResetCache(length: number) {
-    const ref = useRef<VariableSizeList>(null);
-    useEffect(() => {
-      if (ref.current) {
-        ref.current.resetAfterIndex(0, true);
-      }
-    }, [length]);
-    return ref;
-  }
 
   const ListboxComponent = forwardRef<
     HTMLDivElement,
@@ -381,16 +268,24 @@ const Autocomplete = <
       (item: ReactElement & { children?: ReactElement[] }) =>
         [item].concat(item.children || []),
     );
+    const sizeMap = useMemo(() => new Map<number, number>(), []);
+    const virtualizedListRef = useRef<VariableSizeList>(null);
 
-    // the height of an Odyssey autocomplete option item that is used to calculate height of window
-    const optionHeight = 45; //px
+    const setItemSize = useCallback<SetItemSize>(
+      (index, size) => {
+        virtualizedListRef?.current?.resetAfterIndex(0, true);
+        sizeMap.set(index, size);
+      },
+      [virtualizedListRef, sizeMap],
+    );
 
     // The number of items (rows or columns) to render outside of the visible area for performance and scrolling reasons
     const overscanRowCount = 8;
 
-    const itemSize = useCallback(() => optionHeight, []);
-
-    const gridRef = useResetCache(itemData.length);
+    const getItemSize = useCallback(
+      (index: number) => sizeMap.get(index) || 45,
+      [sizeMap],
+    );
 
     const renderWindow = useCallback(
       ({ height, width }: AutoSizerSize) => (
@@ -399,17 +294,24 @@ const Autocomplete = <
           itemData={itemData}
           itemCount={itemData.length}
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          itemSize={itemSize}
+          itemSize={getItemSize}
           height={height}
           width={width}
-          ref={gridRef}
+          ref={virtualizedListRef}
           outerElementType={OuterListboxElementType}
           overscanCount={overscanRowCount}
         >
-          {renderVirtualizedRow}
+          {({ data, index, style }) => (
+            <Row
+              data={data}
+              index={index}
+              style={style}
+              setItemSize={setItemSize}
+            />
+          )}
         </VariableSizeList>
       ),
-      [itemData, gridRef, itemSize],
+      [itemData, getItemSize, setItemSize, virtualizedListRef],
     );
 
     return (
@@ -465,8 +367,10 @@ const Autocomplete = <
       disabled={isDisabled}
       freeSolo={isCustomValueAllowed}
       filterSelectedOptions={true}
-      id={idOverride}
       fullWidth={isFullWidth}
+      getOptionLabel={getOptionLabel}
+      id={idOverride}
+      isOptionEqualToValue={getIsOptionEqualToValue}
       loading={isLoading}
       multiple={hasMultipleChoices}
       onBlur={onBlur}
@@ -476,15 +380,18 @@ const Autocomplete = <
       options={options}
       readOnly={isReadOnly}
       renderInput={renderInput}
-      isOptionEqualToValue={getIsOptionEqualToValue}
+      renderOption={renderOption}
+      renderTags={renderTags}
       translate={translate}
     />
   );
 };
 
 // Need the `typeof Autocomplete` because generics don't get passed through
-const MemoizedAutocomplete = memo(Autocomplete) as typeof Autocomplete;
-// @ts-expect-error displayName is expected to not be on `typeof Autocomplete`
-MemoizedAutocomplete.displayName = "Autocomplete";
+const MemoizedCustomizableAutocomplete = memo(
+  CustomizableAutocomplete,
+) as typeof CustomizableAutocomplete;
+// @ts-expect-error displayName is expected to not be on `typeof CustomizableAutocomplete`
+MemoizedCustomizableAutocomplete.displayName = "CustomizableAutocomplete";
 
-export { MemoizedAutocomplete as Autocomplete };
+export { MemoizedCustomizableAutocomplete as CustomizableAutocomplete };
