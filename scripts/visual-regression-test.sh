4e5f18e27bd7ab7fd4994a0aea5e3b798563f954
Kevin Ghadyani <kevin.ghadyani@okta.com>
2024-05-24 16:09:33 -0500

    fix: updates visual regression test script

--- scripts/visual-regression-test.sh
+++ scripts/visual-regression-test.sh
@@ -9,14 +9,13 @@ export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/unit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
-function get_secret_key_for_applitools() {
-  get_vault_secret_key team-uicore/odyssey ${APPLITOOLS_API_SECRET_IN_VAULT} APPLITOOLS_API_KEY
-  if [[ -z "$APPLITOOLS_API_KEY" ]]; then
-    echo "Error in getting APPLITOOLS_API_KEY from Vault. APPLITOOLS_API_SECRET_IN_VAULT"
-    report_results FAILURE PUBLISH_TYPE_AND_RESULT_DIR_BUT_SUCCEED_IF_NO_RESULTS
-    exit 1
-  fi
-}
+get_terminus_secret "/" APPLITOOLS_API_KEY APPLITOOLS_API_KEY
+
+if [[ -z "$APPLITOOLS_API_KEY" ]]; then
+  echo "Error in getting APPLITOOLS_API_KEY from Vault. APPLITOOLS_API_SECRET_IN_VAULT"
+  report_results FAILURE PUBLISH_TYPE_AND_RESULT_DIR_BUT_SUCCEED_IF_NO_RESULTS
+  exit 1
+fi
 
 if ! yarn workspace @okta/odyssey-storybook ci:visualRegressionTest; then
   echo "lerna tests failed! Exiting..."
