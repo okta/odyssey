eae8e81274bd1a171ae26cc14d6d204905135337
Kevin Ghadyani <kevin.ghadyani@okta.com>
2024-07-19 15:45:23 -0500

    build: minor fix to Unit test script logging

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -10,7 +10,7 @@ echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
 if ! yarn test; then
-  echo "lerna tests failed! Exiting..."
+  echo "Unit tests failed! Exiting..."
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
