d89ebdba2e25fe360a7be13befac307f7e33c78f
Kevin Ghadyani <kevin.ghadyani@okta.com>
2024-07-31 11:43:43 -0500

    build: moves back to older Puppeteer that works with Applitools with possible fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,6 +13,9 @@ fi
 
 cd ${OKTA_HOME}/${REPO}
 
+# Temporary Puppeteer fix until Applitools upgrades their version to >22.1.0 https://github.com/puppeteer/puppeteer/issues/12094#issuecomment-1999345951
+export PUPPETEER_DOWNLOAD_BASE_URL="https://storage.googleapis.com/chrome-for-testing-public"
+
 if ! yarn install --immutable; then
   echo "yarn install command failed! Exiting..."
   exit ${FAILED_SETUP}
