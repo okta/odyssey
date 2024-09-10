c94dad16d8f5efaec09ab2bbe7a977853f04aafe
benlister-okta <ben.lister@okta.com>
2024-09-10 13:08:17 -0700

    fix: add border to banner to fix gray bg contrast

--- packages/odyssey-react-mui/src/theme/components.tsx
+++ packages/odyssey-react-mui/src/theme/components.tsx
@@ -253,7 +253,6 @@ export const components = ({
             justifyContent: "center",
             alignItems: "center",
             borderRadius: 0,
-            border: 0,
 
             ...(ownerState.onClose !== undefined && {
               paddingInline: odysseyTokens.Spacing6,
