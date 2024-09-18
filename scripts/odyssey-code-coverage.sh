c17e90cc73fdb12b2bd9f7b86771d36e618230de
Kevin Ghadyani <kevin.ghadyani@okta.com>
2024-09-18 11:45:50 -0500

    build: fixed code coverage task

--- scripts/odyssey-code-coverage.sh
+++ scripts/odyssey-code-coverage.sh
@@ -8,14 +8,14 @@ cd $OKTA_HOME/$REPO
 export TEST_SUITE_TYPE="junit" # Bacon required config
 
 export CODE_COVERAGE=$(yarn workspace @okta/odyssey-storybook ci:coverage)
-# executes the interaction tests against a local instance of Odyssey storybook running
-if [[ ! -z ${CODE_COVERAGE} ]]; then
+export EXIT_CODE=$?
+
+if [[ $EXIT_CODE -ne 0 ]]; then
   echo "Code coverage failure!"
   report_results FAILURE publish_type_and_result_dir_but_always_fail
   exit "$BUILD_FAILURE"
 fi
 
-# Tests were successful if it reached here
 echo "Code coverage passed!"
 report_results SUCCESS publish_type_and_result_dir_but_succeed_if_no_results
 
