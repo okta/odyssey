69e431e19dcc2e36306eff28e738cc7cfea43b3e
Kevin Ghadyani <kevin.ghadyani@okta.com>
2024-09-18 12:38:15 -0500

    build: echos output of CODE_COVERAGE

--- scripts/odyssey-code-coverage.sh
+++ scripts/odyssey-code-coverage.sh
@@ -10,6 +10,8 @@ export TEST_SUITE_TYPE="junit" # Bacon required config
 export CODE_COVERAGE=$(yarn workspace @okta/odyssey-storybook ci:coverage)
 export EXIT_CODE=$?
 
+echo $CODE_COVERAGE
+
 if [[ $EXIT_CODE -ne 0 ]]; then
   echo "Code coverage failure!"
   report_results FAILURE publish_type_and_result_dir_but_always_fail
@@ -19,4 +21,4 @@ fi
 echo "Code coverage passed!"
 report_results SUCCESS publish_type_and_result_dir_but_succeed_if_no_results
 
-log_custom_message "Code Coverage Report" $CODE_COVERAGE
+log_custom_message "Code Coverage Report" "$CODE_COVERAGE"
