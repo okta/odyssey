7c205f26339b8db080b768adaea0a9d2af67bf5e
bryancunningham-okta <bryan.cunningham@okta.com>
2024-10-21 16:26:17 -0400

    chore(odyssey-react-mui): remove unneeded prop and fragment

--- packages/odyssey-react-mui/src/labs/OdysseyPickers/PickerWithOptionAdornment.tsx
+++ packages/odyssey-react-mui/src/labs/OdysseyPickers/PickerWithOptionAdornment.tsx
@@ -147,7 +147,7 @@ const OptionAdornment = ({
         adornmentSize={adornmentSize}
         odysseyDesignTokens={odysseyDesignTokens}
       >
-        <>{adornment}</>
+        {adornment}
       </OptionAdornmentContainer>
     );
   }
