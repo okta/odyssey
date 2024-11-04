d4cbd70371fbeaba18fb1007b0134fda5abf8b99
Kevin Ghadyani <kevin.ghadyani@okta.com>
2024-11-04 14:06:01 -0600

    fix: removes unused packages

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -10,4 +10,8 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-module.exports = require("@okta/odyssey-lint-staged");
+module.exports = {
+  "*": "prettier --ignore-unknown --loglevel warn --write",
+  "*.scss": "stylelint",
+  "*.{js,jsx,ts,tsx}": "eslint",
+};
