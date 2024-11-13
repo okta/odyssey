a1a8c440f41939fbae983d62598e1c0ea9e3ab9d
bryancunningham-okta <bryan.cunningham@okta.com>
2024-11-13 10:37:13 -0500

    feat(odyssey-react-mui): add some more basic tests

--- packages/odyssey-react-mui/src/labs/DatePickers/DateTimePicker.test.tsx
+++ packages/odyssey-react-mui/src/labs/DatePickers/DateTimePicker.test.tsx
@@ -10,24 +10,8 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  within,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { DateTimePicker } from "./DateTimePicker";
-import { OdysseyProvider } from "../../OdysseyProvider";
-// import {
-//   data,
-//   columns,
-// } from "../../../../odyssey-storybook/src/components/odyssey-labs/DataView/personData";
-// import { filterData } from "../../../../odyssey-storybook/src/components/odyssey-labs/DataView/dataFunctions";
-
-// const getData = ({ ...props }) => {
-//   return filterData({ data, ...props });
-// };
 
 jest.mock("react-i18next", () => ({
   // this mock makes sure any components using the translate hook can use it without a warning being shown
@@ -47,117 +31,36 @@ jest.mock("react-i18next", () => ({
 
 describe("DateTimePicker", () => {
   it("displays the DateTimePicker", async () => {
+    render(<DateTimePicker label="date time picker label" />);
+
+    const input = await screen.getByLabelText("date time picker label");
+    expect(input).toBeInTheDocument();
+  });
+
+  it("displays the correct date and time when a value is passed in ", async () => {
     render(
-      <DateTimePicker label="date time picker label" />
-    )
+      <DateTimePicker
+        label="date time picker label"
+        value="2024-07-11T03:00:00.000Z"
+        timeZone="America/New_York"
+      />,
+    );
 
-     const input = await screen.getByLabelText("date time picker label");
-     expect(input).toBeInTheDocument();
-  })
-})
+    const input = await screen.getByLabelText("date time picker label");
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveDisplayValue("07/10/2024 11:00 PM");
+  });
 
-//   // TODO: Figure out why this test broke when switching to happy-dom.
-//   it.skip("displays the expected number of rows on load more", async () => {
-//     render(
-//       <DataView
-//         availableLayouts={["table"]}
-//         getData={getData}
-//         tableLayoutOptions={{
-//           columns: columns,
-//         }}
-//         hasSearch
-//         hasPagination
-//         // virtualization has to be false for the tests to work properly
-//         enableVirtualization={false}
-//         paginationType="loadMore"
-//         resultsPerPage={20}
-//       />,
-//     );
+  it("displays the correct date and time when timezone is changed", async () => {
+    render(
+      <DateTimePicker
+        label="date time picker label"
+        value="2024-07-11T03:00:00.000Z"
+        timeZone="America/Los_Angeles"
+      />,
+    );
 
-//     const tableElement = await screen.findByRole("table", { name: "" });
-//     const rowElements = within(tableElement).getAllByRole("row", {
-//       hidden: false,
-//     });
-//     expect(rowElements.length).toBe(21);
-
-//     waitFor(() => {
-//       fireEvent.click(screen.getByText("Show more"));
-
-//       const loadedRows = within(tableElement).getAllByRole("row", {
-//         hidden: false,
-//       });
-//       expect(loadedRows.length).toBe(41);
-//     });
-//   });
-
-//   // TODO: Figure out why this test broke when switching to happy-dom.
-//   it.skip("resets the rows when searching", async () => {
-//     render(
-//       <DataView
-//         availableLayouts={["table"]}
-//         getData={getData}
-//         tableLayoutOptions={{
-//           columns: columns,
-//         }}
-//         hasSearch
-//         hasPagination
-//         // virtualization has to be false for the tests to work properly
-//         enableVirtualization={false}
-//         paginationType="loadMore"
-//         resultsPerPage={20}
-//       />,
-//     );
-
-//     const tableElement = await screen.findByRole("table", { name: "" });
-//     const rowElements = within(tableElement).getAllByRole("row", {
-//       hidden: false,
-//     });
-//     expect(rowElements.length).toBe(21);
-
-//     fireEvent.click(screen.getByText("Show more"));
-
-//     waitFor(() => {
-//       const loadedRows = within(tableElement).getAllByRole("row", {
-//         hidden: false,
-//       });
-//       expect(loadedRows.length).toBe(41);
-//     });
-
-//     const searchField = screen.getByPlaceholderText("Search");
-//     fireEvent.change(searchField, { target: { value: "John" } });
-
-//     waitFor(() => {
-//       const rowsAfterFilter = within(tableElement).getAllByRole("row", {
-//         hidden: false,
-//       });
-//       expect(rowsAfterFilter.length).toBeLessThanOrEqual(21);
-//     });
-//   });
-
-//   it("fires onPaginationChange when pagination changes", async () => {
-//     let currentPage = 1;
-//     const onPaginationChange = (pagination: {
-//       pageIndex: number;
-//       pageSize: number;
-//     }) => {
-//       currentPage = pagination.pageIndex;
-//     };
-
-//     render(
-//       <>
-//         <DataView
-//           getData={getData}
-//           hasPagination
-//           onPaginationChange={onPaginationChange}
-//         />
-//       </>,
-//     );
-
-//     const nextButton = screen.getByLabelText("Next page");
-//     fireEvent.click(nextButton);
-
-//     await waitFor(() => {
-//       expect(currentPage).toBe(2);
-//     });
-//   });
-// });
+    const input = screen.getByLabelText("date time picker label");
+    expect(input).toHaveDisplayValue("07/10/2024 08:00 PM");
+  });
+});
