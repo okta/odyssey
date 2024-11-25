17b14cd67c48a3dad0817b046773a63483663114
Ben Schell <ben.schell@okta.com>
2024-11-25 10:09:34 -0500

    fix: attempt to fix VRT on bacon

--- scripts/visual-regression-test.sh
+++ scripts/visual-regression-test.sh
@@ -19,21 +19,21 @@ if [[ -z "$APPLITOOLS_API_KEY" ]]; then
 fi
 
 # Fetch open pull requests
-GITHUB_RESPONSE=$(curl -s -L \
+export GITHUB_RESPONSE=$(curl -s -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$GITHUB_ORG/$REPO/commits/$SHA/pulls")
 
-BASE_BRANCH_NAME=$(echo $GITHUB_RESPONSE | jq -r '.[0].base.ref')
-COMMIT_MESSAGE=$(echo $GITHUB_RESPONSE | jq -r '.[0].body')
-PR_NUMBER=$(echo $GITHUB_RESPONSE | jq -r '.[0].number')
-PR_TITLE=$(echo $GITHUB_RESPONSE | jq -r '.[0].title')
-PR_URL=$(echo $GITHUB_RESPONSE | jq -r '.[0].html_url')
+export BASE_BRANCH_NAME=$(echo $GITHUB_RESPONSE | jq -r '.[0].base.ref')
+export COMMIT_MESSAGE=$(echo $GITHUB_RESPONSE | jq -r '.[0].body')
+export PR_NUMBER=$(echo $GITHUB_RESPONSE | jq -r '.[0].number')
+export PR_TITLE=$(echo $GITHUB_RESPONSE | jq -r '.[0].title')
+export PR_URL=$(echo $GITHUB_RESPONSE | jq -r '.[0].html_url')
 
-APPLITOOLS_BATCH_ID=$SHA
-CURRENT_BRANCH_NAME=$BRANCH
-CHROME_VERSION="126.0.6478.55"
+export APPLITOOLS_BATCH_ID=$SHA
+export CURRENT_BRANCH_NAME=$BRANCH
+export CHROME_VERSION="126.0.6478.55"
 
 if ! setup_service google-chrome-stable ${CHROME_VERSION}-1 ; then
   echo "failure" > ${setup_chrome_status_file}
