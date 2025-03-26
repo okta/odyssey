f68e3ace59ffeb5a58544da3716f2b5e729cbbfe
Kevin Ghadyani <kevin.ghadyani@okta.com>
2025-03-26 15:33:07 -0500

    fix: adds unit tests to useUiShellBreakpoints

--- packages/odyssey-react-mui/src/ui-shell/useUiShellBreakpoints.ts
+++ packages/odyssey-react-mui/src/ui-shell/useUiShellBreakpoints.ts
@@ -12,50 +12,51 @@
 
 import { useMediaQuery } from "../theme/useMediaQuery.js";
 
-export type UiShellBreakpointConfig = Record<
-  "comfortable" | "compact" | "constrained",
-  number
->;
+export type UiShellBreakpointConfig = {
+  narrow: number;
+  medium: number;
+  wide: number;
+};
 
 export const defaultUiShellBreakpointConfig = {
-  comfortable: 800,
-  compact: 600,
-  constrained: 0,
+  narrow: 0,
+  medium: 600,
+  wide: 800,
 } as const satisfies UiShellBreakpointConfig;
 
 export const adminAppUiShellBreakpoints = {
-  comfortable: 1304,
-  compact: 600,
-  constrained: 0,
+  narrow: 0,
+  medium: 600,
+  wide: 1304,
 } as const satisfies UiShellBreakpointConfig;
 
 export const useUiShellBreakpoints = (
   breakpointConfig: UiShellBreakpointConfig = defaultUiShellBreakpointConfig,
 ): keyof UiShellBreakpointConfig | "none" => {
   const isConstrainedView = useMediaQuery(
-    `(min-width: ${breakpointConfig.constrained}px)`,
+    `(min-width: ${breakpointConfig.narrow}px)`,
   );
 
   const isCompactView = useMediaQuery(
-    `(min-width: ${breakpointConfig.compact}px)`,
+    `(min-width: ${breakpointConfig.medium}px)`,
   );
 
   const isComfortableView = useMediaQuery(
-    `(min-width: ${breakpointConfig.comfortable}px)`,
+    `(min-width: ${breakpointConfig.wide}px)`,
   );
 
   if (isComfortableView) {
-    return "comfortable";
+    return "wide";
   }
 
   if (isCompactView) {
-    return "compact";
+    return "medium";
   }
 
   if (isConstrainedView) {
-    return "constrained";
+    return "narrow";
   }
 
-  // For the initial page load as we don't have media queries until the page loads.
+  // This is a failsafe in case we don't have any media queries on page load or if media queries were improperly defined (no `0` case). With the default breakpoints, it will never be `"none"`.
   return "none";
 };
