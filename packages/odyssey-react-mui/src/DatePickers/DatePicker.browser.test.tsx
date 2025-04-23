b8c8a76fc7f5b7c68ea14a25ecde1f72bdf37242
Kevin Ghadyani <kevin.ghadyani@okta.com>
2025-04-23 14:10:22 -0500

    test: fixed DatePicker and DateTimePicker broken tests
    Tests were mocking `react-i18next` globally causing other tests to randomly fail.

--- packages/odyssey-react-mui/src/DatePickers/DatePicker.browser.test.tsx
+++ packages/odyssey-react-mui/src/DatePickers/DatePicker.browser.test.tsx
@@ -11,35 +11,41 @@
  */
 
 import { render, screen } from "@testing-library/react";
-import { DatePicker } from "./DatePicker.js";
 
-vitest.mock("react-i18next", () => ({
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  useTranslation: () => {
-    return {
-      t: vi.fn((string: string) => string),
-      i18n: {
-        language: "en",
-      },
-    };
-  },
-  initReactI18next: {
-    type: "3rdParty",
-    init: () => {},
-  },
-}));
+const getMockedDatePicker = async () => {
+  vi.doMock("react-i18next", () => ({
+    useTranslation: () => {
+      return {
+        t: vi.fn((value: string) => value),
+        i18n: {
+          language: "en",
+        },
+      };
+    },
+    initReactI18next: {
+      type: "3rdParty",
+      init: () => {},
+    },
+  }));
 
-describe("DateTimePicker", () => {
-  it("displays the DateTimePicker", () => {
-    render(<DatePicker label="date time picker label" />);
+  return (await import("./DatePicker.js")).DatePicker;
+};
+
+describe("DatePicker", () => {
+  it("displays the DatePicker", async () => {
+    const MockedDatePicker = await getMockedDatePicker();
+
+    render(<MockedDatePicker label="date time picker label" />);
 
     const input = screen.getByLabelText("date time picker label");
     expect(input).toBeInTheDocument();
   });
 
-  it("displays the correct date when a value is passed in ", () => {
+  it("displays the correct date when a value is passed in ", async () => {
+    const MockedDatePicker = await getMockedDatePicker();
+
     render(
-      <DatePicker
+      <MockedDatePicker
         label="date time picker label"
         value="2024-07-21T03:00:00.000Z"
         timeZone="America/New_York"
@@ -51,9 +57,11 @@ describe("DateTimePicker", () => {
     expect(input).toHaveDisplayValue("07/20/2024");
   });
 
-  it("displays the correct date when timezone is changed", () => {
+  it("displays the correct date when timezone is changed", async () => {
+    const MockedDatePicker = await getMockedDatePicker();
+
     render(
-      <DatePicker
+      <MockedDatePicker
         label="date time picker label"
         value="2024-07-21T03:00:00.000Z"
         timeZone="Asia/Hong_Kong"
